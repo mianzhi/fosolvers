6d27fbf082ca98d29132e2de0207c0b92bdee437
wangmianzhi1@hotmail.com <wangmianzhi1@linuxmail.org>
2011-08-01 13:55:33 -0400

    removed the test program

--- libfosolvers/grid.f90
+++ libfosolvers/grid.f90
@@ -453,33 +453,3 @@ function find4PVol(P1,P2,P3,P4)
   &         -(P1(1)*(y2z3-y3z2)-P2(1)*(y1z3-y3z1)+P3(1)*(y1z2-y2z1)))/6d0
   find4PVol=abs(find4PVol)
 end function
-
-program test
-  use moduleGrid
-  double precision rst1(3),rst2(3)
-  allocate(Node(8))
-  Node(1)%Pos=[0,0,0]
-  Node(2)%Pos=[1,0,0]
-  Node(3)%Pos=[1,1,0]
-  Node(4)%Pos=[0,1,0]
-  Node(5)%Pos=[0,0,1]
-  Node(6)%Pos=[1,0,1]
-  Node(7)%Pos=[1,1,1]
-  Node(8)%Pos=[0,1,1]
-  allocate(Tet(1))
-  Tet(1)%NodeInd(:)=[1,2,5,8]
-  Tet(1)%GeoEnti=10
-  allocate(Hex(1))
-  Hex(1)%NodeInd(:)=[1,2,3,4,5,6,7,8]
-  Hex(1)%GeoEnti=11
-  allocate(Ele(2))
-  Ele(1)%ShapeType=5
-  Ele(1)%ShapeInd=1
-  Ele(2)%ShapeType=4
-  Ele(2)%ShapeInd=1
-  call Ele(1)%findPC(rst1)
-  call Ele(2)%findPC(rst2)
-  write(*,*),rst1,rst2
-  write(*,*),Ele(1)%findVol(),Ele(2)%findVol()
-  write(*,*),Ele(1)%findGeoEnti(),Ele(2)%findGeoEnti()
-end program
