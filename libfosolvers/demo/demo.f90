baceb3ecb3106357d27edc29d92d7a4a38587202
Mianzhi Wang <wangmianzhi1@linuxmail.org>
2011-11-02 19:19:09 -0400

    parallel demo

--- libfosolvers/demo/demo.f90
+++ libfosolvers/demo/demo.f90
@@ -1,23 +1,49 @@
 program libtest
   use moduleGrid
   use moduleWrite
+  use moduleMPIvar
   
-  double precision,allocatable::v(:),vv(:,:),vvv(:,:,:)
-  double precision g(3),gg(3,3)
+  double precision,target,allocatable::v(:),vv(:,:)
+  double precision g(3)
   
-  call readmsh('./grid.msh',10)
-  call sortEle()
-  call updateFacetPara()
-  call updateElePara()
-  call initWriteEnv(0,0,0,1,1,1)
+  call initMPI()
+  call genPrtDataTab(0,0,0,1,1,0)
   
-  allocate(v(nEle))
-  allocate(vv(nEle,3))
-  allocate(vvv(nEle,3,3))
-  
-  tFinal=5d0
-  do l=0,50
-    t=dble(l)/10d0
+  if(pidMPI==0)then
+    call readmsh('./grid.msh',10)
+    call updateFacetPara()
+    call updateElePara()
+    
+    ! prepare data space
+    allocate(v(nEle))
+    v(:)=0d0
+    transEleScal(1)%ptr=>v
+    allocate(vv(nEle,3))
+    vv(:,:)=0d0
+    transEleVect(1)%ptr=>vv
+    
+    ! assign task
+    call distriPrt(1,1)
+    
+    ! gather data
+    call gathData(i)
+    
+    ! write result
+    call initWriteEnv(0,0,0,1,1,0)
+    rstEleScal(1)%ptr=>v
+    rstEleVect(1)%ptr=>vv
+    call writerst('rst.msh',11,.false.)
+  else
+    ! receive task
+    call recvPrt()
+    allocate(v(nEle))
+    v=transEleScal(1)%ptr
+    deallocate(transEleScal(1)%ptr)
+    allocate(vv(nEle,3))
+    vv=transEleVect(1)%ptr
+    deallocate(transEleVect(1)%ptr)
+    
+    ! do work
     do i=1,nEle
       v(i)=sin(t+10d0*Ele(i)%PC(1))+cos(5d0*Ele(i)%PC(2))-sin(5d0*Ele(i)%PC(3))
     end do
@@ -25,16 +51,12 @@ program libtest
       call findEleGradScal(i,v,g)
       vv(i,:)=g(:)
     end do
-    do i=1,nEle
-      call findEleGradVect(i,vv,gg)
-      vvv(i,:,:)=gg(:,:)
-    end do
-    rstEleScal(1,:)=v(:)
-    rstEleVect(1,:,:)=vv(:,:)
-    rstEleTens(1,:,1:3)=vvv(:,1,:)
-    rstEleTens(1,:,4:6)=vvv(:,2,:)
-    rstEleTens(1,:,7:9)=vvv(:,3,:)
-    call writerst('rst.msh',11,.true.)
-  end do
-  call writerst('snap.msh',12,.false.)
+    
+    ! return data
+    transEleScal(1)%ptr=>v
+    transEleVect(1)%ptr=>vv
+    call retnData()
+  end if
+  
+  call finalMPI()
 end program
