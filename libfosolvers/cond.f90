10bac4fc0f9cd6c42355a342c2ec96ae4444d8da
Mianzhi Wang <wangmianzhi1@linuxmail.org>
2011-11-10 14:23:12 -0500

    implemented deconstruction of conditions

--- libfosolvers/cond.f90
+++ libfosolvers/cond.f90
@@ -23,11 +23,15 @@ module moduleCond
     type(typeCond),public,allocatable::Cond(:)
   contains
     procedure,public::getSpace=>getCondSpace
+    procedure,public::clean=>cleanCond
   end type
   type(typeCondList),public,allocatable,save::CondNode(:),CondFacet(:),CondEle(:)
   
 contains
   
+  !-----------------------------------------------
+  ! get a new space for inserting a new condition
+  !-----------------------------------------------
   function getCondSpace(this)
     class(typeCondList),intent(inout)::this
     integer getCondSpace
@@ -45,4 +49,24 @@ contains
       getCondSpace=1
     end if
   end function
+  
+  !-----------------------------------------
+  ! deallocate all components of conditions
+  !-----------------------------------------
+  subroutine cleanCond(this)
+    class(typeCondList),intent(inout)::this
+    
+    if(allocated(this%Cond))then
+      n=size(this%Cond)
+      do i=1,n
+        if(allocated(this%Cond(i)%Val))then
+          deallocate(this%Cond(i)%Val)
+        end if
+        if(allocated(this%Cond(i)%Tab))then
+          deallocate(this%Cond(i)%Tab)
+        end if
+      end do
+      deallocate(this%Cond)
+    end if
+  end subroutine
 end module
