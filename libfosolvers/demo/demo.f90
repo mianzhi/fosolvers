fd0d06d2163b5ed9b3cb7c45ffec9dedcd103041
Mianzhi Wang <wangmianzhi1@linuxmail.org>
2011-12-09 23:50:46 -0500

    implemented data item

--- libfosolvers/demo/demo.f90
+++ libfosolvers/demo/demo.f90
@@ -5,26 +5,26 @@ program demo
   use moduleFileIO
   use moduleMiscDataStruct
   
-!  class(typeDataSet),pointer::vp(3)
-!  type(typeDataSetVal)::v
-!  v%Val=2
-!  allocate(vp,source=v)
-!  write(*,*),vp(1)%lookup()
+  type(typeDataItem)::v
+  call v%specify(TAB1D_TYPE,5)
+  v%Tab1d(:,1)=[1d0,2d0,3d0,4d0,5d0]
+  v%Tab1d(:,2)=[1d1,2d1,3d1,4d1,5d1]
+  write(*,*),v%lookup(-2d0)
   
-  double precision,target,allocatable::v(:),vp(:,:)
-  
-  call readmsh('grid.msh',50,verbose=.true.)
-  
-  allocate(v(nFacet))
-  allocate(vp(nNode,DIMS))
-  call addWrite(v)
-  call addWrite(vp)
-  do i=1,nFacet
-    v(i)=norm2(Facet(i)%PC)
-  end do
-  do i=1,nNode
-    vp(i,:)=Node(i)%Pos(:)
-  end do
-  
-  call writerst('rst.msh',55)
+!  double precision,target,allocatable::v(:),vp(:,:)
+!  
+!  call readmsh('grid.msh',50,verbose=.true.)
+!  
+!  allocate(v(nFacet))
+!  allocate(vp(nNode,DIMS))
+!  call addWrite(v)
+!  call addWrite(vp)
+!  do i=1,nFacet
+!    v(i)=norm2(Facet(i)%PC)
+!  end do
+!  do i=1,nNode
+!    vp(i,:)=Node(i)%Pos(:)
+!  end do
+!  
+!  call writerst('rst.msh',55)
 end program
--- libfosolvers/misc.f90
+++ libfosolvers/misc.f90
@@ -6,21 +6,37 @@
 module moduleMiscDataStruct
   private
   
+  ! constants
+  integer,parameter,private::DATA_NAME_LENGTH=3
+  integer,parameter,public::VAL_TYPE=1
+  integer,parameter,public::TAB1D_TYPE=2
+  
   !-------------------
   ! pointers to array
   !-------------------
-  type::typePtrScalArray
+  type,public::typePtrScalArray
     double precision,pointer::ptr(:)
   end type
-  public::typePtrScalArray
-  type::typePtrVectArray
+  type,public::typePtrVectArray
     double precision,pointer::ptr(:,:)
   end type
-  public::typePtrVectArray
-  type::typePtrTensArray
+  type,public::typePtrTensArray
     double precision,pointer::ptr(:,:,:)
   end type
-  public::typePtrTensArray
+  
+  !----------
+  ! data set
+  !----------
+  type,public::typeDataItem
+    character(DATA_NAME_LENGTH) DataName
+    integer DataType
+    double precision Val
+    double precision,allocatable::Tab1d(:,:)
+  contains
+    procedure,public::specify=>specifyDataItem
+    procedure,public::lookup=>lookupDataItem
+    !TODO: final::cleanDataItem
+  end type
   
   !------------------------------------
   ! extend a generic allocatable array
@@ -66,6 +82,80 @@ contains
       call move_alloc(temp,arr)
     else
       allocate(arr(l))
+    end if
+  end subroutine
+  
+  !------------------------------------
+  ! specify at type for this data item
+  !------------------------------------
+  elemental subroutine specifyDataItem(this,datatype,n)
+    class(typeDataItem),intent(inout)::this
+    integer,intent(in)::datatype
+    integer,intent(in),optional::n
+    integer tablength
+    
+    this%DataType=datatype
+    if(allocated(this%Tab1d))then
+      deallocate(this%Tab1d)
+    end if
+    select case(datatype)
+      case(VAL_TYPE)
+        ! nothing to do
+      case(TAB1D_TYPE)
+        if(present(n))then
+          tablength=n
+        else
+          tablength=0
+        end if
+        allocate(this%Tab1d(tablength,2))
+      case default
+    end select
+  end subroutine
+  
+  !----------------------------------
+  ! lookup value from this data item
+  !----------------------------------
+  elemental function lookupDataItem(this,x)
+    class(typeDataItem),intent(in)::this
+    double precision,intent(in),optional::x
+    double precision lookupDataItem
+    double precision pos
+    
+    lookupDataItem=0d0
+    select case(this%DataType)
+      case(VAL_TYPE)
+        lookupDataItem=this%Val
+      case(TAB1D_TYPE)
+        if(present(x))then
+          pos=x
+        else
+          pos=0d0
+        end if
+        do i=2,size(this%Tab1d,1)
+          if(pos<this%Tab1d(i,1))then
+            lookupDataItem=this%Tab1d(i-1,2)+(pos-this%Tab1d(i-1,1))&
+            &                                *(this%Tab1d(i,2)-this%Tab1d(i-1,2))&
+            &                                /(this%Tab1d(i,1)-this%Tab1d(i-1,1))
+            exit
+          end if
+        end do
+        if(i>size(this%Tab1d,1))then
+          lookupDataItem=this%Tab1d(i-2,2)+(pos-this%Tab1d(i-2,1))&
+          &                                *(this%Tab1d(i-1,2)-this%Tab1d(i-2,2))&
+          &                                /(this%Tab1d(i-1,1)-this%Tab1d(i-2,1))
+        end if
+      case default
+    end select
+  end function
+  
+  !----------------------------
+  ! destructor of typeDataItem
+  !----------------------------
+  elemental subroutine cleanDataItem(this)
+    type(typeDataItem),intent(inout)::this
+    
+    if(allocated(this%Tab1d))then
+      deallocate(this%Tab1d)
     end if
   end subroutine
   
