1c8f41680ea70698928bbe21996cdddb057e1348
Mianzhi Wang <wangmianzhi1@linuxmail.org>
2012-04-15 19:55:54 -0400

    + implemented list of double vectors

--- src/libfosolvers/BasicDataStruct/moduleBasicDataStruct.f90
+++ src/libfosolvers/BasicDataStruct/moduleBasicDataStruct.f90
@@ -7,7 +7,7 @@ module moduleBasicDataStruct
   !> list of integer scalars
   type,public::typeListIScal
     integer,allocatable::dat(:) !< data pool
-    integer::length !< length of the data pool that is used
+    integer::len !< length of the data pool that is used
   contains
     procedure,public::clear=>clearListIScal
     procedure,public::extend=>extendListIScal
@@ -25,7 +25,7 @@ module moduleBasicDataStruct
   !> list of double scalars
   type,public::typeListDScal
     double precision,allocatable::dat(:) !< data pool
-    integer::length !< length of the data pool that is used
+    integer::len !< length of the data pool that is used
   contains
     procedure,public::clear=>clearListDScal
     procedure,public::extend=>extendListDScal
@@ -38,6 +38,26 @@ module moduleBasicDataStruct
     procedure,public::addto=>addListDScaltoPack
     procedure,public::recover=>recoverListDScalfromPack
     !FIXME:final::purgeListDScal
+  end type
+  
+  !> list of double vectors
+  type,public::typeListDVect
+    double precision,allocatable::dat(:,:) !< data pool
+    integer::dim !< dimension of the vectors
+    integer::len !< length of the data pool that is used
+  contains
+    procedure,public::clear=>clearListDVect
+    procedure,public::specDim=>specListDVectDim
+    procedure,public::extend=>extendListDVect
+    generic,public::push=>pushListDVectOne,pushListDVectMulti
+      procedure::pushListDVectOne
+      procedure::pushListDVectMulti
+    generic,public::get=>getListDVectOne,getListDVectMulti
+      procedure::getListDVectOne
+      procedure::getListDVectMulti
+    procedure,public::addto=>addListDVecttoPack
+    procedure,public::recover=>recoverListDVectfromPack
+    !FIXME:final::purgeListDVect
   end type
   
   !> serialized data package
@@ -57,7 +77,7 @@ contains
   elemental subroutine clearListIScal(this)
     class(typeListIScal),intent(inout)::this
     
-    this%length=0
+    this%len=0
     if(allocated(this%dat))then
       deallocate(this%dat)
     end if
@@ -73,7 +93,7 @@ contains
       m=size(this%dat)
     else
       m=0
-      this%length=0
+      this%len=0
     end if
     if(present(n))then
       allocate(newDat(m+n))
@@ -95,14 +115,14 @@ contains
       m=size(this%dat)
     else
       m=0
-      this%length=0
+      this%len=0
     end if
-    do while(this%length+1>m)
+    do while(this%len+1>m)
       call this%extend()
       m=size(this%dat)
     end do
-    this%dat(this%length+1)=val
-    this%length=this%length+1
+    this%dat(this%len+1)=val
+    this%len=this%len+1
   end subroutine
   
   !> push multiple val to the back of this ListIScal
@@ -114,15 +134,15 @@ contains
       m=size(this%dat)
     else
       m=0
-      this%length=0
+      this%len=0
     end if
     n=size(val)
-    do while(this%length+n>m)
+    do while(this%len+n>m)
       call this%extend()
       m=size(this%dat)
     end do
-    this%dat(this%length+1:this%length+n)=val(:)
-    this%length=this%length+n
+    this%dat(this%len+1:this%len+n)=val(:)
+    this%len=this%len+n
   end subroutine
   
   !> get n_th val from this ListIScal
@@ -149,8 +169,8 @@ contains
     type(typeSerialPack),intent(inout)::sp !< target package
     
     if(allocated(this%dat))then
-      call sp%iDat%push(this%length)
-      call sp%iDat%push(this%dat(1:this%length))
+      call sp%iDat%push(this%len)
+      call sp%iDat%push(this%dat(1:this%len))
     else
       call sp%iDat%push(0)
     end if
@@ -164,7 +184,7 @@ contains
     call this%clear()
     sp%iPtr=sp%iPtr+1
     m=sp%iDat%get(sp%iPtr)
-    this%length=m
+    this%len=m
     if(m>0)then
       call this%extend(m)
       call this%push(sp%iDat%get([(sp%iPtr+i,i=1,m)]))
@@ -174,7 +194,7 @@ contains
   
   !> destructor of ListIScal
   elemental subroutine purgeListIScal(this)
-    type(typeListIScal),intent(inout)::this !< this ListDScal
+    type(typeListIScal),intent(inout)::this !< this ListIScal
     
     if(allocated(this%dat))then
       deallocate(this%dat)
@@ -185,7 +205,7 @@ contains
   elemental subroutine clearListDScal(this)
     class(typeListDScal),intent(inout)::this !< this ListDScal
     
-    this%length=0
+    this%len=0
     if(allocated(this%dat))then
       deallocate(this%dat)
     end if
@@ -201,7 +221,7 @@ contains
       m=size(this%dat)
     else
       m=0
-      this%length=0
+      this%len=0
     end if
     if(present(n))then
       allocate(newDat(m+n))
@@ -223,14 +243,14 @@ contains
       m=size(this%dat)
     else
       m=0
-      this%length=0
+      this%len=0
     end if
-    do while(this%length+1>m)
+    do while(this%len+1>m)
       call this%extend()
       m=size(this%dat)
     end do
-    this%dat(this%length+1)=val
-    this%length=this%length+1
+    this%dat(this%len+1)=val
+    this%len=this%len+1
   end subroutine
   
   !> push multiple val to the back of this ListDScal
@@ -242,15 +262,15 @@ contains
       m=size(this%dat)
     else
       m=0
-      this%length=0
+      this%len=0
     end if
     n=size(val)
-    do while(this%length+n>m)
+    do while(this%len+n>m)
       call this%extend()
       m=size(this%dat)
     end do
-    this%dat(this%length+1:this%length+n)=val(:)
-    this%length=this%length+n
+    this%dat(this%len+1:this%len+n)=val(:)
+    this%len=this%len+n
   end subroutine
   
   !> get n_th val from this ListDScal
@@ -277,8 +297,8 @@ contains
     type(typeSerialPack),intent(inout)::sp !< target package
     
     if(allocated(this%dat))then
-      call sp%iDat%push(this%length)
-      call sp%dDat%push(this%dat(1:this%length))
+      call sp%iDat%push(this%len)
+      call sp%dDat%push(this%dat(1:this%len))
     else
       call sp%iDat%push(0)
     end if
@@ -286,13 +306,13 @@ contains
   
   !> recover this ListDScal from serialized data package sp
   subroutine recoverListDScalfromPack(this,sp)
-    class(typeListDScal),intent(inout)::this !< this ListIScal
+    class(typeListDScal),intent(inout)::this !< this ListDScal
     type(typeSerialPack),intent(inout)::sp !< source package
     
     call this%clear()
     sp%iPtr=sp%iPtr+1
     m=sp%iDat%get(sp%iPtr)
-    this%length=m
+    this%len=m
     if(m>0)then
       call this%extend(m)
       call this%push(sp%dDat%get([(sp%dPtr+i,i=1,m)]))
@@ -303,6 +323,151 @@ contains
   !> destructor of ListDScal
   elemental subroutine purgeListDScal(this)
     type(typeListDScal),intent(inout)::this !< this ListDScal
+    
+    if(allocated(this%dat))then
+      deallocate(this%dat)
+    end if
+  end subroutine
+  
+  !> clear this ListDVect
+  elemental subroutine clearListDVect(this)
+    class(typeListDVect),intent(inout)::this !< this ListDVect
+    
+    this%dim=0
+    this%len=0
+    if(allocated(this%dat))then
+      deallocate(this%dat)
+    end if
+  end subroutine
+  
+  !> specify the vectors in this ListDVect to be n dimensional
+  elemental subroutine specListDVectDim(this,n)
+    class(typeListDVect),intent(inout)::this !< this ListDVect
+    integer,intent(in)::n !< vector dimension
+    
+    this%dim=n
+  end subroutine
+  
+  !> extend this ListDVect by doubling size of dat or add n spaces
+  elemental subroutine extendListDVect(this,n)
+    class(typeListDVect),intent(inout)::this !< this ListDVect
+    integer,intent(in),optional::n !< number of entries expected to be extended
+    double precision,allocatable::newDat(:,:)
+    
+    if(allocated(this%dat))then
+      m=size(this%dat,2)
+    else
+      m=0
+      this%len=0
+    end if
+    if(present(n))then
+      allocate(newDat(this%dim,m+n))
+    else
+      allocate(newDat(this%dim,max(1,(m*2))))
+    end if
+    if(m>0)then
+      newDat(:,1:m)=this%dat(:,:)
+    end if
+    call move_alloc(newDat,this%dat)
+  end subroutine
+  
+  !> push one vector to the back of this ListDVect
+  subroutine pushListDVectOne(this,vector)
+    class(typeListDVect),intent(inout)::this !< this ListDVect
+    double precision,intent(in)::vector(this%dim) !< vector to be pushed in
+    
+    if(allocated(this%dat))then
+      m=size(this%dat,2)
+    else
+      m=0
+      this%len=0
+    end if
+    do while(this%len+1>m)
+      call this%extend()
+      m=size(this%dat,2)
+    end do
+    this%dat(:,this%len+1)=vector(:)
+    this%len=this%len+1
+  end subroutine
+  
+  !> push multiple vectors to the back of this ListDVect
+  subroutine pushListDVectMulti(this,vectors)
+    class(typeListDVect),intent(inout)::this !< this ListDVect
+    double precision,intent(in)::vectors(:,:) !< vectors to be pushed in
+    
+    if(allocated(this%dat))then
+      m=size(this%dat,2)
+    else
+      m=0
+      this%len=0
+    end if
+    n=size(vectors,2)
+    do while(this%len+n>m)
+      call this%extend()
+      m=size(this%dat,2)
+    end do
+    this%dat(:,this%len+1:this%len+n)=vectors(:,:)
+    this%len=this%len+n
+  end subroutine
+  
+  !> get n_th vector from this ListDVect
+  function getListDVectOne(this,n) result(vector)
+    class(typeListDVect),intent(in)::this !< this ListDVect
+    integer,intent(in)::n !< vector index to be looked up
+    double precision vector(this%dim)
+    
+    vector(:)=this%dat(:,n)
+  end function
+  
+  !> get multiple vectors from this ListDVect
+  function getListDVectMulti(this,list) result(vectors)
+    class(typeListDVect),intent(in)::this !< this ListDVect
+    integer,intent(in)::list(:) !< vector index to be looked up
+    double precision vectors(this%dim,size(list))
+    
+    vectors(:,:)=this%dat(:,list(:))
+  end function
+  
+  !> add this ListDVect to serialized data package sp
+  subroutine addListDVecttoPack(this,sp)
+    class(typeListDVect),intent(in)::this !< this ListDVect
+    type(typeSerialPack),intent(inout)::sp !< target package
+    
+    call sp%iDat%push(this%dim)
+    if(allocated(this%dat))then
+      call sp%iDat%push(this%len)
+      do i=1,this%len
+        call sp%dDat%push(this%dat(:,i))
+      end do
+    else
+      call sp%iDat%push(0)
+    end if
+  end subroutine
+  
+  !> recover this ListDVect from serialized data package sp
+  subroutine recoverListDVectfromPack(this,sp)
+    class(typeListDVect),intent(inout)::this !< this ListDVect
+    type(typeSerialPack),intent(inout)::sp !< source package
+    
+    call this%clear()
+    sp%iPtr=sp%iPtr+1
+    k=sp%iDat%get(sp%iPtr)
+    this%dim=k
+    sp%iPtr=sp%iPtr+1
+    m=sp%iDat%get(sp%iPtr)
+    this%len=m
+    if(m>0)then
+      call this%extend(m)
+      do i=1,m
+        call this%push(sp%dDat%get([(sp%dPtr+j,j=1,k)]))
+        sp%dPtr=sp%dPtr+k
+      end do
+    end if
+  end subroutine
+  
+  !> destructor of ListDVect
+  elemental subroutine purgeListDVect(this)
+    type(typeListDVect),intent(inout)::this !< this ListDVect
     
     if(allocated(this%dat))then
       deallocate(this%dat)
