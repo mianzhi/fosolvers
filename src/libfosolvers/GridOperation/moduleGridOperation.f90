fe7e64cbdde6857b3e1b9a0acbb6745fb8e00236
Mianzhi Wang <wangmianzhi1@linuxmail.org>
2012-06-12 17:20:43 +0800

    + generated the partition mapping for point, line, facet and block

--- src/libfosolvers/GridOperation/moduleGridOperation.f90
+++ src/libfosolvers/GridOperation/moduleGridOperation.f90
@@ -5,18 +5,79 @@ module moduleGridOperation
   use moduleGrid
   private
   
+  ! constants
+  integer,parameter::NPLFB=5 !< node, point, line, facet & block, 5 things need to be mapped
+  integer,parameter,public::MAP_NODE=1 !< map index of node
+  integer,parameter,public::MAP_POINT=2 !< map index of Point
+  integer,parameter,public::MAP_LINE=3 !< map index of Line
+  integer,parameter,public::MAP_FACET=4 !< map index of Facet
+  integer,parameter,public::MAP_BLOCK=5 !< map index of Block
+  
   ! public procedures
   public::splitGridPrt
   
 contains
   
   !> split the original grid into resulting grids with respect to partition
-  subroutine splitGridPrt(origin,rst)
-    type(typeGrid),intent(in)::origin !< the original grid
+  subroutine splitGridPrt(org,rst,r2o,isOverlap)
+    use moduleBasicDataStruct
+    type(typeGrid),intent(in)::org !< the original grid
     type(typeGrid),intent(inout),allocatable::rst(:) !< the resulting grids
+    type(typeHtr1DIArr),intent(inout),allocatable::r2o(:,:) !< mapping from rst to org
+    logical,intent(in),optional::isOverlap
+    logical overlap
     
+    if(present(isOverlap))then
+      overlap=isOverlap
+    else
+      overlap=.false.
+    end if
     if(allocated(rst)) deallocate(rst)
-    allocate(rst(origin%nPrt))
+    if(allocated(r2o)) deallocate(r2o)
+    allocate(rst(org%nPrt))
+    allocate(r2o(NPLFB,org%nPrt))
+    do i=1,org%nPrt
+      call rst(i)%init()
+    end do
+    do i=1,org%nPoint
+      do j=1,size(org%Point(i)%Prt)
+        if(org%Point(i)%Prt(j)/=0)then
+          call pushArr(r2o(MAP_POINT,org%Point(i)%Prt(j))%dat,i)
+          rst(org%Point(i)%Prt(j))%nPoint=rst(org%Point(i)%Prt(j))%nPoint+1
+        end if
+      end do
+    end do
+    do i=1,org%nLine
+      do j=1,size(org%Line(i)%Prt)
+        if(org%Line(i)%Prt(j)/=0)then
+          call pushArr(r2o(MAP_LINE,org%Line(i)%Prt(j))%dat,i)
+          rst(org%Line(i)%Prt(j))%nLine=rst(org%Line(i)%Prt(j))%nLine+1
+        end if
+      end do
+    end do
+    do i=1,org%nFacet
+      do j=1,size(org%Facet(i)%Prt)
+        if(org%Facet(i)%Prt(j)/=0)then
+          call pushArr(r2o(MAP_FACET,org%Facet(i)%Prt(j))%dat,i)
+          rst(org%Facet(i)%Prt(j))%nFacet=rst(org%Facet(i)%Prt(j))%nFacet+1
+        end if
+      end do
+    end do
+    do i=1,org%nBlock
+      do j=1,size(org%Block(i)%Prt)
+        if(overlap)then
+          if(org%Block(i)%Prt(j)/=0)then
+            call pushArr(r2o(MAP_BLOCK,abs(org%Block(i)%Prt(j)))%dat,i)
+            rst(abs(org%Block(i)%Prt(j)))%nBlock=rst(abs(org%Block(i)%Prt(j)))%nBlock+1
+          end if
+        else
+          if(org%Block(i)%Prt(j)>0)then
+            call pushArr(r2o(MAP_BLOCK,org%Block(i)%Prt(j))%dat,i)
+            rst(org%Block(i)%Prt(j))%nBlock=rst(org%Block(i)%Prt(j))%nBlock+1
+          end if
+        end if
+      end do
+    end do
   end subroutine
   
 end module
--- src/test/test.f90
+++ src/test/test.f90
@@ -5,14 +5,24 @@ program tests
 end program
 
 subroutine testReadGMSH()
+  use moduleBasicDataStruct
   use moduleFileIO
   use moduleGrid
+  use moduleGridOperation
   type(typeGrid)::grid
+  type(typeGrid),allocatable::sgrid(:)
+  type(typeHtr1DIArr),allocatable::map(:,:)
   
   open(12,file='bin/gridGMSH1.msh',status='old')
   call readGMSH(12,grid)
   close(12)
-  write(*,*),grid%nPrt
+  call splitGridPrt(grid,sgrid,map,isOverlap=.false.)
+  do i=1,grid%nPrt
+    write(*,*),i,'::',size(map(MAP_BLOCK,i)%dat)
+    do j=1,sgrid(i)%nBlock
+      write(*,*),'    ',j,map(MAP_BLOCK,i)%dat(j)
+    end do
+  end do
   open(13,file='rst.msh',status='replace')
   call writeGMSH(13,grid)
   close(13)
