8e90e42f6d226ddc98ae283438f9f0cb1182a4e2
Mianzhi Wang <wangmianzhi1@linuxmail.org>
2012-06-25 10:36:17 +0800

    + validated convection

--- src/test/test.f90
+++ src/test/test.f90
@@ -26,31 +26,30 @@ subroutine testReadGMSH()
   allocate(vv(DIMS,grid%nBlock))
   call grid%updateBlockPos()
   do i=1,grid%nBlock
-    v(i)=merge(100d0,100d0,grid%BlockPos(1,i)<0.5d0)
+    v(i)=merge(100d0,000d0,grid%BlockPos(1,i)<0.1d0)
   end do
   allocate(u(DIMS,grid%nIntf))
-  !do i=1,grid%nIntf
-  !  u(:,i)=[1d0,0d0,0d0]
-  !end do
+  do i=1,grid%nIntf
+    u(:,i)=[1d0,0d0,0d0]
+  end do
   t=0d0
   open(13,file='rst.msh',status='replace')
   call writeGMSH(13,grid)
   call writeGMSH(13,v,grid,BIND_BLOCK,'name1',0,t)
   call grid%updateBlockVol()
-  do i=1,10000
+  do i=1,100
     vv=findGrad(v,grid,BIND_BLOCK)
-    tempv=v+2d-5*findDiffus(v,grid,vv)/grid%BlockVol(:)
+    tempv=v+5d-3*findConvect(v,u,BIND_INTF,grid,vv,limiter=minmod)/grid%BlockVol(:)
     do j=1,801,100
-      tempv(j)=tempv(j)-2d0*v(j)/1d-4*2d-5
+      tempv(j)=100d0
     end do
     do j=100,900,100
-      tempv(j)=tempv(j)-(v(j)+dot_product(vv(:,j),[1d-2,0d0,0d0]))/1d-2*2d-5
+      tempv(j)=tempv(j)
     end do
     v(:)=tempv(:)
-    t=t+2d-5
-    if(mod(i,100)==0)then
+    t=t+5d-3
+    if(mod(i,1)==0)then
       call writeGMSH(13,v,grid,BIND_BLOCK,'name1',i,t)
-      write(*,*),i
     end if
   end do
   close(13)
