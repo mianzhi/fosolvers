dfb77f99480fb647b3a1a3ce08f5cefc90f05fab
Mianzhi Wang <wangmianzhi1@linuxmail.org>
2012-06-27 11:51:42 +0800

    + finished sending and receiving intrinsic data

--- src/libfosolvers/MultiProcessing/moduleMPIComm.f90
+++ src/libfosolvers/MultiProcessing/moduleMPIComm.f90
@@ -28,9 +28,13 @@ module moduleMPIComm
     module procedure::recvIScal
     module procedure::recvDScal
     module procedure::recvIVect
+    module procedure::recvIVectRealloc
     module procedure::recvDVect
+    module procedure::recvDVectRealloc
     module procedure::recvIMat
+    module procedure::recvIMatRealloc
     module procedure::recvDMat
+    module procedure::recvDMatRealloc
   end interface
   public::recvDat
   
@@ -95,123 +99,203 @@ contains
   end subroutine
   
   !> send arbitrary-rank integer array
-  subroutine sendIAnyRankArr(arr,sizeArr,dest)
+  subroutine sendIArr(arr,n,dest)
     use mpi
     integer,intent(in)::arr(*) !< the integer array to be sent
-    integer,intent(in)::sizeArr(:) !< the size of all dimensions of arr
+    integer,intent(in)::n !< the size of arr
     integer,intent(in)::dest !< the destination process
     integer ierr
     
-    call MPI_send(sizeArr(:),size(sizeArr),MPI_integer,dest,0,MPI_comm_world,ierr)
-    n=product(sizeArr)
+    call MPI_send(n,1,MPI_integer,dest,0,MPI_comm_world,ierr)
     call MPI_send(arr(1:n),n,MPI_integer,dest,0,MPI_comm_world,ierr)
   end subroutine
   
   !> receive arbitrary-rank integer array
-  subroutine recvIAnyRankArr(arr,sizeArr,source)
+  subroutine recvIArr(arr,n,source)
     use mpi
     integer,intent(out)::arr(*) !< the integer array to be sent
-    integer,intent(inout)::sizeArr(:) !< the size of all dimensions of arr
+    integer,intent(out)::n !< the size of arr
     integer,intent(in)::source !< the source process
     integer ierr,stat(MPI_status_size)
     
-    call MPI_recv(sizeArr(:),size(sizeArr),MPI_integer,source,0,MPI_comm_world,stat,ierr)
-    n=product(sizeArr)
+    call MPI_recv(n,1,MPI_integer,source,0,MPI_comm_world,stat,ierr)
     call MPI_recv(arr(1:n),n,MPI_integer,source,0,MPI_comm_world,stat,ierr)
   end subroutine
   
   !> send arbitrary-rank double array
-  subroutine sendDAnyRankArr(arr,sizeArr,dest)
+  subroutine sendDArr(arr,n,dest)
     use mpi
     double precision,intent(in)::arr(*) !< the double array to be sent
-    integer,intent(in)::sizeArr(:) !< the size of all dimensions of arr
+    integer,intent(in)::n !< the size of arr
     integer,intent(in)::dest !< the destination process
     integer ierr
     
-    call MPI_send(sizeArr(:),size(sizeArr),MPI_integer,dest,0,MPI_comm_world,ierr)
-    n=product(sizeArr)
+    call MPI_send(n,1,MPI_integer,dest,0,MPI_comm_world,ierr)
     call MPI_send(arr(1:n),n,MPI_double_precision,dest,0,MPI_comm_world,ierr)
   end subroutine
   
   !> receive arbitrary-rank double array
-  subroutine recvDAnyRankArr(arr,sizeArr,source)
+  subroutine recvDArr(arr,n,source)
     use mpi
     double precision,intent(out)::arr(*) !< the double array to be sent
-    integer,intent(inout)::sizeArr(:) !< the size of all dimensions of arr
+    integer,intent(out)::n !< the size of arr
     integer,intent(in)::source !< the source process
     integer ierr,stat(MPI_status_size)
     
-    call MPI_recv(sizeArr(:),size(sizeArr),MPI_integer,source,0,MPI_comm_world,stat,ierr)
-    n=product(sizeArr)
+    call MPI_recv(n,1,MPI_integer,source,0,MPI_comm_world,stat,ierr)
     call MPI_recv(arr(1:n),n,MPI_double_precision,source,0,MPI_comm_world,stat,ierr)
   end subroutine
   
   !> send integer vector object
   subroutine sendIVect(obj,dest)
+    use mpi
     integer,intent(in)::obj(:) !< the integer vector to be sent
     integer,intent(in)::dest !< the destination process
+    integer ierr
     
-    call sendIAnyRankArr(obj,[size(obj)],dest)
+    call MPI_send(size(obj),1,MPI_integer,dest,0,MPI_comm_world,ierr)
+    call sendIArr(obj,size(obj),dest)
   end subroutine
   
   !> receive integer vector object
   subroutine recvIVect(obj,source)
-    integer,intent(inout)::obj(:) !< the integer vector to be received
+    use mpi
+    integer,intent(out)::obj(:) !< the integer vector to be received
     integer,intent(in)::source !< the source process
-    integer sz(1)
+    integer ierr,stat(MPI_status_size)
     
-    call recvIAnyRankArr(obj,sz,source)
+    call MPI_recv(n,1,MPI_integer,source,0,MPI_comm_world,stat,ierr)
+    call recvIArr(obj,n,source)
+  end subroutine
+  
+  !> receive integer vector object and reallocate if specified
+  subroutine recvIVectRealloc(obj,source,realloc)
+    use mpi
+    use moduleBasicDataStruct
+    integer,intent(inout),allocatable::obj(:) !< the integer vector to be received
+    integer,intent(in)::source !< the source process
+    logical,intent(in)::realloc !< whether the obj should be reallocated
+    integer ierr,stat(MPI_status_size)
+    
+    call MPI_recv(n,1,MPI_integer,source,0,MPI_comm_world,stat,ierr)
+    if(realloc)then
+      call reallocArr(obj,n)
+    end if
+    call recvIArr(obj,n,source)
   end subroutine
   
   !> send double vector object
   subroutine sendDVect(obj,dest)
+    use mpi
     double precision,intent(in)::obj(:) !< the double vector to be sent
     integer,intent(in)::dest !< the destination process
+    integer ierr
     
-    call sendDAnyRankArr(obj,[size(obj)],dest)
+    call MPI_send(size(obj),1,MPI_integer,dest,0,MPI_comm_world,ierr)
+    call sendDArr(obj,size(obj),dest)
   end subroutine
   
   !> receive double vector object
   subroutine recvDVect(obj,source)
-    double precision,intent(inout)::obj(:) !< the double vector to be received
+    use mpi
+    double precision,intent(out)::obj(:) !< the double vector to be received
     integer,intent(in)::source !< the source process
-    integer sz(1)
+    integer ierr,stat(MPI_status_size)
     
-    call recvDAnyRankArr(obj,sz,source)
+    call MPI_recv(n,1,MPI_integer,source,0,MPI_comm_world,stat,ierr)
+    call recvDArr(obj,n,source)
+  end subroutine
+  
+  !> receive double vector object and reallocate if specified
+  subroutine recvDVectRealloc(obj,source,realloc)
+    use mpi
+    use moduleBasicDataStruct
+    double precision,intent(inout),allocatable::obj(:) !< the double vector to be received
+    integer,intent(in)::source !< the source process
+    logical,intent(in)::realloc !< whether the obj should be reallocated
+    integer ierr,stat(MPI_status_size)
+    
+    call MPI_recv(n,1,MPI_integer,source,0,MPI_comm_world,stat,ierr)
+    if(realloc)then
+      call reallocArr(obj,n)
+    end if
+    call recvDArr(obj,n,source)
   end subroutine
   
   !> send integer matrix object
   subroutine sendIMat(obj,dest)
+    use mpi
     integer,intent(in)::obj(:,:) !< the integer matrix to be sent
     integer,intent(in)::dest !< the destination process
+    integer ierr
     
-    call sendIAnyRankArr(obj,[size(obj,1),size(obj,2)],dest)
+    call MPI_send([size(obj,1),size(obj,2)],2,MPI_integer,dest,0,MPI_comm_world,ierr)
+    call sendIArr(obj,size(obj),dest)
   end subroutine
   
   !> receive integer matrix object
   subroutine recvIMat(obj,source)
-    integer,intent(inout)::obj(:,:) !< the integer matrix to be received
+    use mpi
+    integer,intent(out)::obj(:,:) !< the integer matrix to be received
     integer,intent(in)::source !< the source process
-    integer sz(2)
+    integer ierr,stat(MPI_status_size),shp(2)
     
-    call recvIAnyRankArr(obj,sz,source)
+    call MPI_recv(shp,2,MPI_integer,source,0,MPI_comm_world,stat,ierr)
+    call recvIArr(obj,n,source)
+  end subroutine
+  
+  !> receive integer matrix object and reallocate if specified
+  subroutine recvIMatRealloc(obj,source,realloc)
+    use mpi
+    use moduleBasicDataStruct
+    integer,intent(inout),allocatable::obj(:,:) !< the integer matrix to be received
+    integer,intent(in)::source !< the source process
+    logical,intent(in)::realloc !< whether the obj should be reallocated
+    integer ierr,stat(MPI_status_size),shp(2)
+    
+    call MPI_recv(shp,2,MPI_integer,source,0,MPI_comm_world,stat,ierr)
+    if(realloc)then
+      call reallocArr(obj,shp(1),shp(2))
+    end if
+    call recvIArr(obj,n,source)
   end subroutine
   
   !> send double matrix object
   subroutine sendDMat(obj,dest)
+    use mpi
     double precision,intent(in)::obj(:,:) !< the double matrix to be sent
     integer,intent(in)::dest !< the destination process
+    integer ierr
     
-    call sendDAnyRankArr(obj,[size(obj,1),size(obj,2)],dest)
+    call MPI_send([size(obj,1),size(obj,2)],2,MPI_integer,dest,0,MPI_comm_world,ierr)
+    call sendDArr(obj,size(obj),dest)
   end subroutine
   
   !> receive double matrix object
   subroutine recvDMat(obj,source)
-    double precision,intent(inout)::obj(:,:) !< the double matrix to be received
+    use mpi
+    double precision,intent(out)::obj(:,:) !< the double matrix to be received
     integer,intent(in)::source !< the source process
-    integer sz(2)
+    integer ierr,stat(MPI_status_size),shp(2)
     
-    call recvDAnyRankArr(obj,sz,source)
+    call MPI_recv(shp,2,MPI_integer,source,0,MPI_comm_world,stat,ierr)
+    call recvDArr(obj,n,source)
+  end subroutine
+  
+  !> receive double matrix object and reallocate if specified
+  subroutine recvDMatRealloc(obj,source,realloc)
+    use mpi
+    use moduleBasicDataStruct
+    double precision,intent(inout),allocatable::obj(:,:) !< the double matrix to be received
+    integer,intent(in)::source !< the source process
+    logical,intent(in)::realloc !< whether the obj should be reallocated
+    integer ierr,stat(MPI_status_size),shp(2)
+    
+    call MPI_recv(shp,2,MPI_integer,source,0,MPI_comm_world,stat,ierr)
+    if(realloc)then
+      call reallocArr(obj,shp(1),shp(2))
+    end if
+    call recvDArr(obj,n,source)
   end subroutine
   
 end module
--- src/test/test.f90
+++ src/test/test.f90
@@ -11,8 +11,10 @@ program test
   use moduleInterpolation
   use moduleMPIComm
   type(typeGrid)::grid
-  integer a,v(2,3)
-  double precision b,d(3)
+  integer a,c(2),e(4,2)
+  double precision b,d(3),f(3,2)
+  integer,allocatable::cc(:),ee(:,:)
+  double precision,allocatable::dd(:),ff(:,:)
   
   call initMPI()
   if(pidMPI==0)then
@@ -20,21 +22,47 @@ program test
     call readGMSH(12,grid)
     close(12)
     a=2
-    b=3d0
-    v=reshape([1,2,3,4,5,6],[2,3])
-    d=[1d0,2d0,3d0]
+    b=2.4d0
+    c=[4,2]
+    d=[4d0,7d0,19d0]
+    e=reshape([1,2,3,4,5,6,7,8],[4,2])
+    f=reshape([1d0,2d0,3d0,4d0,5d0,6d0],[3,2])
     call sendDat(a,1)
     call sendDat(b,1)
-    call sendDat(v,1)
+    call sendDat(c,1)
+    call sendDat(c,1)
     call sendDat(d,1)
+    call sendDat(d,1)
+    call sendDat(e,1)
+    call sendDat(e,1)
+    call sendDat(f,1)
+    call sendDat(f,1)
   else
     call recvDat(a,0)
     call recvDat(b,0)
-    call recvDat(v,0)
+    call recvDat(c,0)
+    call recvDat(cc,0,realloc=.true.)
     call recvDat(d,0)
-    write(*,*),a,b
-    write(*,*),v(:,1),v(:,2),v(:,3)
+    call recvDat(dd,0,realloc=.true.)
+    call recvDat(e,0)
+    call recvDat(ee,0,realloc=.true.)
+    call recvDat(f,0)
+    call recvDat(ff,0,realloc=.true.)
+    write(*,*),a
+    write(*,*),b
+    write(*,*),c
+    write(*,*),cc
     write(*,*),d
+    write(*,*),dd
+    do i=1,size(e,2)
+      write(*,*),e(:,i)
+      write(*,*),ee(:,i)
+    end do
+    do i=1,size(f,2)
+      write(*,*),f(:,i)
+      write(*,*),ff(:,i)
+    end do
+    !write(*,*),v(:,1),v(:,2),v(:,3)
     !open(13,file='rst.msh',status='replace')
     !call writeGMSH(13,grid)
     !close(13)
