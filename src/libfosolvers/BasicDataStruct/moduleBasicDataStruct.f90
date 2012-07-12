d4b2cd3ee4e7839a2dc6917e7796efa4410c85c3
Mianzhi Wang <wangmianzhi1@linuxmail.org>
2012-07-12 15:52:17 +0800

    + implemented general data linked list (need to implement file parser)

--- src/libfosolvers/BasicDataStruct/moduleBasicDataStruct.f90
+++ src/libfosolvers/BasicDataStruct/moduleBasicDataStruct.f90
@@ -4,11 +4,30 @@
 module moduleBasicDataStruct
   private
   
+  ! constants
+  integer,parameter,public::CONST_TYPE=0 !< constant type
+  integer,parameter,public::TAB1D_TYPE=1 !< 1d table type
+  
   !> heterogeneous 1D integer allocatable array
   type,public::typeHtr1DIArr
     integer,allocatable::dat(:) !< 1D integer allocatable array
   contains
     !FIXME:final::purgeHtr1DIArr
+  end type
+  
+  !> general data linked list
+  type,public::typeGenDatLlist
+    !TODO: change to character(:) (waiting new gcc)
+    character(1),allocatable::key(:) !< key of the data item
+    type(typeGenDatLlist),pointer::next=>null() !< the next data item in the list
+    integer datType !< type of data
+    double precision,allocatable::dat(:,:) !< raw data
+  contains
+    procedure,public::push=>pushGenDatLlist
+    procedure,public::test=>testGenDatLlist
+    procedure,public::get=>getGenDatLlist
+    procedure,public::clear=>clearGenDatLlist
+    !FIXME:final::purgeGenDatLlist
   end type
   
   !> reallocate a generic allocatable array
@@ -41,6 +60,134 @@ contains
     type(typeHtr1DIArr),intent(inout)::this !< this 1D integer allocatable array
     
     if(allocated(this%dat)) deallocate(this%dat)
+  end subroutine
+  
+  !> push item to general data linked list
+  recursive pure subroutine pushGenDatLlist(this,item)
+    class(typeGenDatLlist),intent(inout)::this !< this linked list item
+    type(typeGenDatLlist),intent(in)::item !< item to be pushed
+    !TODO: change to character(:) (waiting new gcc)
+    character(:),allocatable::thiskey,itemkey
+    if(allocated(this%key))then
+      allocate(character(size(this%key))::thiskey)
+      forall(i=1:size(this%key))
+        thiskey(i:i)=this%key(i)
+      end forall
+    else
+      thiskey=''
+    end if
+    allocate(character(size(item%key))::itemkey)
+    forall(i=1:size(item%key))
+      itemkey(i:i)=item%key(i)
+    end forall
+    
+    if(allocated(this%key))then
+      if(thiskey/=itemkey)then
+        if(.not.associated(this%next))then
+          allocate(this%next)
+        end if
+        call this%next%push(item)
+      end if
+    else
+      allocate(this%key(size(item%key)),source=item%key)
+      this%datType=item%datType
+      !TODO:remove the allocation bounds (waiting new gcc)
+      allocate(this%dat(lbound(item%dat,1):ubound(item%dat,1),lbound(item%dat,2):ubound(item%dat,2)),source=item%dat)
+    end if
+  end subroutine
+  
+  !> test if the key exists in the linked list
+  recursive pure function testGenDatLlist(this,key)
+    class(typeGenDatLlist),intent(in)::this !< this linked list item
+    character(*),intent(in)::key !< the key to be tested
+    logical testGenDatLlist !< whether the key exists in this linked list
+    !TODO: change to character(:) (waiting new gcc)
+    character(:),allocatable::thiskey
+    allocate(character(size(this%key))::thiskey)
+    forall(i=1:size(this%key))
+      thiskey(i:i)=this%key(i)
+    end forall
+    
+    testGenDatLlist=.false.
+    if((key==thiskey))then
+      testGenDatLlist=.true.
+    else
+      if(associated(this%next))then
+        testGenDatLlist=this%next%test(key)
+      end if
+    end if
+  end function
+  
+  !> get the value according to the key from this linked list
+  recursive pure function getGenDatLlist(this,key,arg)
+    class(typeGenDatLlist),intent(in)::this !< this linked list item
+    character(*),intent(in)::key !< the key to be found
+    double precision,intent(in),optional::arg(*) !< optional arguments
+    double precision getGenDatLlist !< the value associated with the key
+    double precision x
+    !TODO: change to character(:) (waiting new gcc)
+    character(:),allocatable::thiskey
+    allocate(character(size(this%key))::thiskey)
+    forall(i=1:size(this%key))
+      thiskey(i:i)=this%key(i)
+    end forall
+    
+    getGenDatLlist=0d0
+    if(key==thiskey)then
+      select case(this%datType)
+      case(CONST_TYPE)
+        getGenDatLlist=this%dat(1,1)
+      case(TAB1D_TYPE)
+        if(present(arg))then
+          x=arg(1)
+        else
+          x=0d0
+        end if
+        do i=2,size(this%dat,1)
+          if(x<this%dat(i,0))then
+            getGenDatLlist=this%dat(i-1,1)+(x-this%dat(i-1,0))&
+            &                              *(this%dat(i,1)-this%dat(i-1,1))&
+            &                              /(this%dat(i,0)-this%dat(i-1,0))
+            exit
+          end if
+        end do
+        if(i>ubound(this%dat,1))then
+          getGenDatLlist=this%dat(i-2,1)+(x-this%dat(i-2,0))&
+          &                              *(this%dat(i-1,1)-this%dat(i-2,1))&
+          &                              /(this%dat(i-1,0)-this%dat(i-2,0))
+        end if
+      case default
+      end select
+    else
+      if(associated(this%next))then
+        if(present(arg))then
+          getGenDatLlist=this%next%get(key,arg)
+        else
+          getGenDatLlist=this%next%get(key)
+        end if
+      end if
+    end if
+  end function
+  
+  !> clear this linked list
+  recursive pure subroutine clearGenDatLlist(this)
+    class(typeGenDatLlist),intent(inout)::this !< this linked list item
+    
+    if(associated(this%next))then
+      call this%next%clear()
+      nullify(this%next)
+    end if
+    if(allocated(this%key)) deallocate(this%key)
+    if(allocated(this%dat)) deallocate(this%dat)
+  end subroutine
+  
+  !> destructor of GenDatLlist
+  elemental subroutine purgeGenDatLlist(this)
+    type(typeGenDatLlist),intent(inout)::this !< this linked list item
+    
+    if(allocated(this%key)) deallocate(this%key)
+    if(allocated(this%dat)) deallocate(this%dat)
+    if(associated(this%next)) nullify(this%next)
   end subroutine
   
   !> reallocate the 1D integer array arr to have m entries
--- src/test/test.f90
+++ src/test/test.f90
@@ -12,16 +12,23 @@ program test
   use moduleInterpolation
   use moduleMPIComm
   type(typeGrid)::grid
+  type(typeGenDatLlist)::ll,temp
   
   call initMPI()
   if(pidMPI==0)then
     open(12,file='bin/gridGMSH4.msh',status='old')
     call readGMSH(12,grid)
     close(12)
-    call grid%updateEdge()
-    do i=1,grid%nEdge
-      write(*,*),i,':',grid%EdgeNeibBlock(i)%dat
-    end do
+    temp%key=['a']
+    temp%datType=TAB1D_TYPE
+    allocate(temp%dat(3,0:1),source=reshape([0d0,1d0,2d0,1d1,2d1,3d1],[3,2]))
+    call ll%push(temp)
+    temp%key=['b','c']
+    call ll%push(temp)
+    temp%key=['d','e','f']
+    call ll%push(temp)
+    write(*,*),ll%test('bc'),ll%test('ef')
+    write(*,*),ll%get('a',[-0.3d0]),ll%get('def',[5.1d0]),ll%get('123')
   else
   end if
   call finalMPI()
