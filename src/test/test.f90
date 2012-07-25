d9d4e00c8a21963b393081f832d89453343d8875
Mianzhi Wang <wangmianzhi1@linuxmail.org>
2012-07-25 20:47:32 +0800

    + new test grid and test case for convection note: 1. convection between dual blocks is BS while convection between blocks is acceptable 2. concervation issue due to change of block volume during sub-cycling   newPhi=(oldPhi*oldVol+flow)/newVol

--- src/test/test.f90
+++ src/test/test.f90
@@ -17,32 +17,34 @@ program test
   
   call initMPI()
   if(pidMPI==0)then
-    open(12,file='bin/gridGMSH4.msh',status='old')
+    open(12,file='bin/gridGMSH5.msh',status='old')
     call readGMSH(12,grid)
     close(12)
-    call grid%updateDualBlock()
+    call grid%updateBlockPos()
     allocate(disp(DIMS,grid%nNode))
-    allocate(v(grid%nNode))
-    allocate(temp(grid%nNode))
-    allocate(gradv(DIMS,grid%nNode))
+    allocate(v(grid%nBlock))
+    allocate(temp(grid%nBlock))
+    allocate(gradv(DIMS,grid%nBlock))
     disp(:,:)=0d0
-    disp(1,:)=0.05d0
-    do i=1,grid%nNode
-      v(i)=merge(100d0,000d0,grid%NodePos(1,i)<0.9d0.and.grid%NodePos(1,i)>0.7d0)
+    forall(i=1:grid%nNode)
+      disp(1,i)=0.05d0-0.04d0*grid%NodePos(1,i)
+    end forall
+    do i=1,grid%nBlock
+      v(i)=merge(100d0,000d0,grid%BlockPos(1,i)<0.9d0.and.grid%BlockPos(1,i)>0.7d0)
     end do
     t=0d0
     open(13,file='rst.msh',status='replace')
     call writeGMSH(13,grid)
-    call writeGMSH(13,v,grid,BIND_NODE,'name1',0,t)
+    call writeGMSH(13,v,grid,BIND_BLOCK,'name1',0,t)
     call writeGMSH(13,0d0*disp,grid,BIND_NODE,'s',0,t)
     do i=1,10
       t=t+1d0
-      gradv=findGrad(v,grid,BIND_NODE)
-      temp=findDispConvect(v,BIND_NODE,disp,grid,gradv,limiter=vanLeer)
-      call grid%updateDualBlock()
-      v=v+temp/grid%NodeVol
+      gradv=findGrad(v,grid,BIND_BLOCK)
+      temp=findDispConvect(v,BIND_BLOCK,disp,grid,gradv,limiter=vanLeer)
       call mvGrid(grid,disp)
-      call writeGMSH(13,v,grid,BIND_NODE,'name1',i,t)
+      call grid%updateBlockVol()
+      v=v+temp/grid%BlockVol
+      call writeGMSH(13,v,grid,BIND_BLOCK,'name1',i,t)
       call writeGMSH(13,dble(i)*disp,grid,BIND_NODE,'s',i,t)
     end do
     close(13)
