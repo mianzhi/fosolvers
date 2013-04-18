fb877dde4a131572d744f71f658c3d2cd6bcba8a
Mianzhi Wang <mianzhi@Verdi>
2013-04-18 09:16:51 -0500

    + life is much easier without the Euler equation. But I still want a decent 1d Euler solver!

--- src/foburn1d/foburn1d.f90
+++ src/foburn1d/foburn1d.f90
@@ -10,100 +10,55 @@ program foburn1d
   use moduleInterpolation
   use moduleCLIO
   
-  call grid%genUniform(0d0,1d0,100)
+  width=0.5d0
+  call grid%genUniform(0d0,width,100)
   call setEnv()
   t=0d0
-  dt=1d-6
-  tFinal=2d-2!8d-4
+  dt=1d-3
+  tFinal=50d0
   ! initial state
   gamm=1.4d0
-  Dm=1d-4
-  nu=1d-4
-  alpha=1d-4
+  Dm=5d-4 ![m^2/s]
+  alpha=10d-4 ![m^2/s]
+  mw=30d-3 ![kg/mol]
+  R=RU/mw ![J/kg/K]
+  Cv=R/(gamm-1d0) ![J/kg/K]
+  Cp=Cv*gamm ![J/kg/K]
+  Q=50d3 ![J/mol reactant]
   Y(:)=1d0
   u(:)=0d0
-  p(:)=1d5 !< inital pressure
+  p=1d5 !< inital pressure
   !p(1:2)=1d6
   Temp(:)=300d0 !< initial temperature
-  Temp(1:4)=2000d0
-  rho(:)=p(:)/300d0/Temp(:) !TODO:rho=rho(p,T), R=300
-  IE(:)=300d0*Temp(:)/(gamm-1d0) !TODO:IE=IE(p,T), R=300
-  uCell=itplNode2Cell(u,grid)
-  E(:)=IE(:)+uCell(:)**2d0/2d0
-  rhoNode=itplCell2Node(rho,grid)
-  rhou(:)=rhoNode(:)*u(:)
-  rhoE(:)=rho(:)*E(:)
+  !Temp(1:4)=2000d0
+  rho(:)=p/R/Temp(:)
   Mass(:)=rho(:)*grid%CellWidth(:)
-  Mom(:)=rhou(:)*grid%NodeWidth(:)
-  Energy(:)=rhoE(:)*grid%CellWidth(:)
-  IEnergy(:)=IE(:)*Mass(:)
+  Temp(1)=2d3
   ! advance in time
   do while(t<tFinal)
-    ! boundary sources
-    forall(i=2:grid%nNode-1)
-      Mom(i)=Mom(i)+dt*(p(NlC(i))-p(NrC(i)))
-    end forall
-    Mom(:)=Mom(:)+dt/3d0*findDiffus(nu*[(1,i=1,grid%nCell)],BIND_NODE,u,grid)
-    pNode=itplCell2Node(p,grid)
-    forall(i=1:grid%nCell)
-      Energy(i)=Energy(i)+dt*&
-      &  ((pNode(ClN(i))+nu*(uCell(ClC(i))-uCell(i))/grid%NodeWidth(ClN(i))/3d0)*u(ClN(i))&
-      &  -(pNode(CrN(i))+nu*(uCell(CrC(i))-uCell(i))/grid%NodeWidth(CrN(i))/3d0)*u(CrN(i)))
-    end forall
-    burnR(:)=1d9*Y(:)*exp(-1.8d4/Temp(:))
-    burnR(:)=min(burnR(:),Y(:)/dt)
-    Energy(:)=Energy(:)+dt*rho(:)*burnR(:)*grid%CellWidth*1.2d6
-    !write(*,*),rho*grid%CellWidth
-    !write(*,*),Energy
-    !write(*,*),burnR
-    !stop
-    Energy(:)=Energy(:)+dt*findDiffus(alpha*[(1,i=1,grid%nCell)],BIND_CELL,Temp,grid)
-    Y(:)=(Y(:)*Mass(:)+dt*findDiffus(Dm*rho(:),BIND_CELL,Y,grid))/Mass(:)
-    Y(:)=Y(:)-dt*burnR(:)
-    Y(:)=max(Y(:),[(0d0,i=1,grid%nCell)])
-    ! closed boundary
-    u(1)=0d0
-    u(grid%nNode)=0d0
-    Mom(1)=0d0
-    Mom(grid%nNode)=0d0
-    ! recover states
-    rhoNode=itplCell2Node(rho,grid)
-    rhou(:)=Mom(:)/grid%NodeWidth(:)
-    u(:)=rhou(:)/rhoNode(:)
-    uCell(:)=itplNode2Cell(u,grid)
-    rhoE(:)=Energy(:)/grid%CellWidth(:)
-    ! convection
-    rhox=findGrad(rho,BIND_CELL,grid)
-    rhoux=findGrad(rhou,BIND_NODE,grid)
-    rhoEx=findGrad(rhoE,BIND_CELL,grid)
-    Mass(:)=Mass(:)+dt*findConvect(rho,u,BIND_NODE,grid,rhox)
-    do i=2,grid%nNode-1 !TODO: needs a node convection scheme
-      if(rhou(i)>0d0)then
-        Mom(i)=Mom(i)+dt*(rhou(i-1)-rhou(i))
-      else
-        Mom(i)=Mom(i)+dt*(rhou(i)-rhou(i+1))
-      end if
+    ! diffusion and burn
+    burnR(:)=1d9*(Y(:)*rho(:)/mw)*exp(-1.5d4/Temp(:))
+    burnR(:)=min(burnR(:),Y(:)*rho(:)/mw/dt)
+    Temp(:)=Temp(:)+dt*findDiffus(alpha*[(1d0,i=1,grid%nCell)],BIND_CELL,Temp,grid)
+    Temp(:)=Temp(:)+dt*Q/Cp*burnR(:)/rho(:)
+    Y(:)=Y(:)+dt*findDiffus(Dm*[(1d0,i=1,grid%nCell)],BIND_CELL,Y,grid)
+    Y(:)=Y(:)-dt*mw*burnR(:)/rho(:)
+    ! move gas
+    rho(:)=p/R/Temp(:)
+    grid%CellWidth(:)=Mass(:)/rho(:)
+    do i=2,grid%nNode
+      grid%NodePos(i)=grid%NodePos(NlN(i))+grid%CellWidth(NlC(i))
     end do
-    Energy(:)=Energy(:)+dt*findConvect(rhoE,u,BIND_NODE,grid,rhoEx)
-    Y(:)=Y(:)+dt*findConvect(Y,u,BIND_NODE,grid,Yx)
-    ! update the new intensive state and internal energy
-    rhou(:)=Mom(:)/grid%NodeWidth(:)
-    rhoE(:)=Energy(:)/grid%CellWidth(:)
-    rhoNode=itplCell2Node(rho,grid)
-    u(:)=rhou(:)/rhoNode(:)
-    E(:)=rhoE(:)/rho(:)
-    uCell=itplNode2Cell(u,grid)
-    IEnergy(:)=Energy(:)-Mass(:)*uCell(:)**2d0/2d0
-    IE(:)=IEnergy/Mass(:)
-    Temp(:)=IE(:)/300d0*(gamm-1d0)
-    p(:)=rho(:)*300d0*Temp(:)
+    p=p*(grid%NodePos(grid%nNode)/width)**gamm
+    Temp(:)=Temp(:)*(grid%NodePos(grid%nNode)/width)**(gamm-1d0)
+    grid%NodePos(:)=grid%NodePos(:)*width/grid%NodePos(grid%nNode)
+    call grid%update()
     rho(:)=Mass(:)/grid%CellWidth(:)
-    !call showProg(t/tFinal)
     t=t+dt
   end do
   
   do i=1,grid%nCell
-    write(*,'(10g11.3)'),grid%CellPos(i),p(i),rho(i),u(i),Temp(i),Y(i)
+    write(*,'(10g11.3)'),grid%CellPos(i),Temp(i),Y(i),rho(i)
   end do
   ! clean up
   call clearEnv()
--- src/foburn1d/miscBurn1D.f90
+++ src/foburn1d/miscBurn1D.f90
@@ -9,31 +9,23 @@ module miscBurn1D
   double precision t !< current time
   double precision dt !< time step size
   double precision tFinal !< simulation time
+  double precision width !< width of the domain
   double precision,allocatable::rho(:) !< density
   double precision,allocatable::Y(:) !< mass fraction of reactant
   double precision,allocatable::u(:) !< velocity
-  double precision,allocatable::p(:) !< pressure
+  double precision,allocatable::p !< pressure
   double precision,allocatable::Temp(:) !< temperature
-  double precision,allocatable::E(:) !< internal_energy and kinetic_energy
-  double precision,allocatable::IE(:) !< internal_energy
-  double precision,allocatable::rhou(:) !< momentum per unit volume
-  double precision,allocatable::rhoE(:) !< rho*E
-  double precision,allocatable::Mass(:) !< cell mass
-  double precision,allocatable::Mom(:) !< extensive cell momentum
-  double precision,allocatable::Energy(:) !< extensive cell energy
-  double precision,allocatable::IEnergy(:) !< extensive cell internal energy
   double precision,allocatable::burnR(:) !< burn rate
-  double precision,allocatable::rhox(:) !< derivative of rho along x
-  double precision,allocatable::rhoux(:) !< derivative of rhou along x
-  double precision,allocatable::rhoEx(:) !< derivative of rhoE along x
-  double precision,allocatable::Yx(:) !< derivative of Y along x
-  double precision,allocatable::pNode(:) !< p at node
-  double precision,allocatable::uCell(:) !< u at cell
-  double precision,allocatable::rhoNode(:) !< rho at node
+  double precision,allocatable::Mass(:) !< mass
   double precision gamm !< ratio of heat capacities
   double precision Dm !< mass diffusivity
-  double precision nu !< kinematic viscosity
   double precision alpha !< thermal diffusivity
+  double precision mw !< molecular weight
+  double precision R !< gas constant for product and reactant
+  double precision Cv,Cp !< heat capacities
+  double precision Q !< heating value
+  
+  double precision,parameter::RU=8.3144621d0 !< universal gas constant
   
 contains
   
@@ -42,24 +34,9 @@ contains
     allocate(rho(grid%nCell))
     allocate(Y(grid%nCell))
     allocate(u(grid%nNode))
-    allocate(p(grid%nCell))
     allocate(Temp(grid%nCell))
-    allocate(E(grid%nCell))
-    allocate(IE(grid%nCell))
-    allocate(rhou(grid%nNode))
-    allocate(rhoE(grid%nCell))
-    allocate(Mass(grid%nCell))
-    allocate(Mom(grid%nNode))
-    allocate(Energy(grid%nCell))
-    allocate(IEnergy(grid%nCell))
     allocate(burnR(grid%nCell))
-    allocate(rhox(grid%nCell))
-    allocate(rhoux(grid%nNode))
-    allocate(rhoEx(grid%nCell))
-    allocate(Yx(grid%nCell))
-    allocate(pNode(grid%nNode))
-    allocate(uCell(grid%nCell))
-    allocate(rhoNode(grid%nNode))
+    allocate(Mass(grid%nCell))
   end subroutine
   
   !> clear environment
@@ -67,24 +44,9 @@ contains
     deallocate(rho)
     deallocate(Y)
     deallocate(u)
-    deallocate(p)
     deallocate(Temp)
-    deallocate(E)
-    deallocate(IE)
-    deallocate(rhou)
-    deallocate(rhoE)
-    deallocate(Mass)
-    deallocate(Mom)
-    deallocate(Energy)
-    deallocate(IEnergy)
     deallocate(burnR)
-    deallocate(rhox)
-    deallocate(rhoux)
-    deallocate(rhoEx)
-    deallocate(Yx)
-    deallocate(pNode)
-    deallocate(uCell)
-    deallocate(rhoNode)
+    deallocate(Mass)
   end subroutine
   
 end module
