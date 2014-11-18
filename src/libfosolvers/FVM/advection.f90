08bffd518d9c5901e3bc8500d0f88c81deca8881
Mianzhi Wang <wangmianzhi1@linuxmail.org>
2014-11-18 15:40:25 -0600

    + used strong conservsative form for advection

--- src/libfosolvers/FVM/advection.f90
+++ src/libfosolvers/FVM/advection.f90
@@ -16,47 +16,44 @@ module modAdvection
   
 contains
   
-  !> find advection of vector s by velocity u on polyFvGrid
-  !> \f[ \int_A \mathbf{s} (\mathbf{u} \cdot \hat{n}) dA \f]
-  subroutine findAdvPolyVect(grid,s,u,adv)
+  !> find advection due to flux f depending on vector s on polyFvGrid
+  !> \f[ \int_A \mathbf{f}(\mathbf{s}) \cdot \hat{n} dA \f]
+  subroutine findAdvPolyVect(grid,s,f,adv)
     use modPolyFvGrid
     use modGradient
     class(polyFvGrid),intent(inout)::grid !< the grid
-    double precision,intent(in)::s(:,:) !< advected values
-    double precision,intent(in)::u(:,:) !< velocity
+    double precision,intent(in)::s(:,:) !< state variables
+    double precision,intent(in)::f(:,:,:) !< fluxes
     double precision,allocatable,intent(inout)::adv(:,:) !< advection output
     integer::up,dn
     double precision::flow(size(s,1)),fUp,fDn,df,r
-    double precision,allocatable::flux(:,:),gradFlux(:,:,:)
+    double precision,allocatable::gradF(:,:,:)
     
     call grid%up()
     if(.not.(allocated(adv)))then
       allocate(adv(size(s,2),grid%nC))
     end if
     adv(:,:)=0d0
-    allocate(flux(DIMS*size(s,1),grid%nC))
-    allocate(gradFlux(DIMS,DIMS*size(s,1),grid%nC))
-    forall(i=1:grid%nC)
-      forall(j=1:size(s,1))
-        flux(j*DIMS-(DIMS-1):j*DIMS,i)=s(j,i)*u(:,i)
-      end forall
-    end forall
-    call findGrad(grid,flux,gradFlux)
+    allocate(gradF(DIMS,DIMS*size(s,1),grid%nC))
+    call findGrad(grid,reshape(f,[size(f,1)*size(f,2),size(f,3)]),gradF)
     do i=1,grid%nP
       m=grid%iEP(1,i)
       n=grid%iEP(2,i)
       if(m<=grid%nC.and.n<=grid%nC)then
-        if(dot_product(u(:,m)+u(:,n),grid%normP(:,i))>=0d0)then
-          up=m
-          dn=n
-        else
-          up=n
-          dn=m
-        end if
         do j=1,size(s,1)
-          fUp=dot_product(flux(j*DIMS-(DIMS-1):j*DIMS,up),grid%normP(:,i))
-          fDn=dot_product(flux(j*DIMS-(DIMS-1):j*DIMS,dn),grid%normP(:,i))
-          df=dot_product(matmul(grid%p(dn)-grid%p(up),gradFlux(:,j*DIMS-(DIMS-1):j*DIMS,up)),&
+          if(abs(s(j,m)-s(j,n))<=tiny(1d0))then
+            up=m
+            dn=n
+          else if(dot_product((f(:,j,m)-f(:,j,n))/(s(j,m)-s(j,n)),grid%normP(:,i))>=0d0)then
+            up=m
+            dn=n
+          else
+            up=n
+            dn=m
+          end if
+          fUp=dot_product(f(:,j,up),grid%normP(:,i))
+          fDn=dot_product(f(:,j,dn),grid%normP(:,i))
+          df=dot_product(matmul(grid%p(dn)-grid%p(up),gradF(:,j*DIMS-(DIMS-1):j*DIMS,up)),&
           &              grid%normP(:,i))
           r=merge(2d0*df/(fDn-fUp)-1d0,fUp,abs(fDn-fUp)>tiny(1d0))
           flow(j)=-grid%aP(i)*(fUp+0.5d0*vanAlbada(r)*(fDn-fUp))
@@ -65,32 +62,34 @@ contains
         adv(:,n)=adv(:,n)-flow(:)
       end if
     end do
-    deallocate(flux)
-    deallocate(gradFlux)
+    deallocate(gradF)
   end subroutine
   
-  !> find advection of scalar s by velocity u on polyFvGrid
-  !> \f[ \int_A s (\mathbf{u} \cdot \hat{n}) dA \f]
-  subroutine findAdvPolyScal(grid,s,u,adv)
+  !> find advection due to flux f depending on scalar s on polyFvGrid
+  !> \f[ \int_A \mathbf{f}(s) \cdot \hat{n} dA \f]
+  subroutine findAdvPolyScal(grid,s,f,adv)
     use modPolyFvGrid
     class(polyFvGrid),intent(inout)::grid !< the grid
-    double precision,intent(in)::s(:) !< advected value
-    double precision,intent(in)::u(:,:) !< velocity
+    double precision,intent(in)::s(:) !< state variable
+    double precision,intent(in)::f(:,:) !< flux
     double precision,allocatable,intent(inout)::adv(:) !< advection output
-    double precision,allocatable::sv(:,:),advv(:,:)
+    double precision,allocatable::sv(:,:),fv(:,:,:),advv(:,:)
     
     allocate(sv(1,size(s)))
+    allocate(fv(DIMS,1,size(s)))
     if(allocated(adv))then
       allocate(advv(1,size(adv)))
     end if
     sv(1,:)=s(:)
-    call findAdvPolyVect(grid,sv,u,advv)
+    fv(:,1,:)=f(:,:)
+    call findAdvPolyVect(grid,sv,fv,advv)
     if(.not.allocated(adv))then
       allocate(adv(size(advv,2)),source=advv(1,:))!FIXME:remove work-around
     else
       adv(:)=advv(1,:)
     end if
     deallocate(sv)
+    deallocate(fv)
     deallocate(advv)
   end subroutine
   
--- src/test/advection1.f90
+++ src/test/advection1.f90
@@ -6,7 +6,7 @@ function advection1() result(ierr)
   use modAdvection
   integer ierr
   type(polyFvGrid)::grid
-  double precision,allocatable::s(:),u(:,:),tmps(:)
+  double precision,allocatable::s(:),u(:,:),f(:,:),tmps(:)
   double precision::p(3),dt
   
   ierr=0
@@ -16,6 +16,7 @@ function advection1() result(ierr)
   call grid%up()
   allocate(s(grid%nC))
   allocate(u(3,grid%nC))
+  allocate(f(3,grid%nC))
   forall(i=1:grid%nC)
     u(:,i)=[1d0,0d0,0d0]
   end forall
@@ -31,7 +32,10 @@ function advection1() result(ierr)
   end do
   dt=0.0005d0
   do l=1,200
-    call findAdv(grid,s,u,tmps)
+    forall(i=1:grid%nC)
+      f(:,i)=s(i)*u(:,i)
+    end forall
+    call findAdv(grid,s,f,tmps)
     s(:)=s(:)+dt*tmps(:)/grid%v(:)
   end do
   open(10,file='advection1_rst.vtk',action='write')
@@ -40,5 +44,5 @@ function advection1() result(ierr)
   call writeVTK(10,'s',[s,[(0d0,i=1,grid%nE-grid%nC)]])
   close(10)
   call grid%clear()
-  deallocate(s,u,tmps)
+  deallocate(s,u,f,tmps)
 end function
