495312c405cd9658b5a8e6ada5321428e0eb332e
Mianzhi Wang <wangmianzhi1@linuxmail.org>
2017-10-08 16:53:24 -0400

    + use grid%near in gradient calculation

--- src/libfosolvers/FVM/gradient.f90
+++ src/libfosolvers/FVM/gradient.f90
@@ -26,85 +26,52 @@ contains
     class(polyFvGrid),intent(inout)::grid !< the grid
     double precision,intent(in)::v(:,:) !< input data
     double precision,allocatable,intent(inout)::gradv(:,:,:) !< gradient output
-    logical::useCellOnly,findCellOnly
-    integer,parameter::MIN_N_NEIB=8
-    integer,parameter::MAX_N_NEIB=25
+    logical::useCellOnly
+    integer,parameter::MAX_N_NEAR=25
     integer,parameter::MAX_L_WORK=2000
     integer,parameter::L_IWORK=200
-    integer::nNeib,iNeib(MAX_N_NEIB),lwork,iwork(L_IWORK),rank,ier
-    double precision::dx(MAX_N_NEIB,DIMS),dv(MAX_N_NEIB,size(v,1)),stat(DIMS),work(MAX_L_WORK),rcond
+    integer::nNear,iNear(MAX_N_NEAR),lwork,iwork(L_IWORK),rank,ier
+    double precision::dx(MAX_N_NEAR,DIMS),dv(MAX_N_NEAR,size(v,1)),stat(DIMS),work(MAX_L_WORK),rcond
     double precision::vF(DIMS),tmp
     
     call grid%up()
     m=size(v,1) ! number of components
     ! handling non-cell elements
     useCellOnly=size(v,2)<grid%nE
-    findCellOnly=.true.
-    if(allocated(gradv))then
-      if(size(gradv,3)>=grid%nE)then
-        findCellOnly=.false.
-      end if
-    else
-      findCellOnly=useCellOnly
-      allocate(gradv(DIMS,m,merge(grid%nC,grid%nE,findCellOnly)))
+    if(.not.allocated(gradv))then
+      allocate(gradv(DIMS,m,grid%nC))
     end if
     ! find gradient
     !$omp parallel do default(shared)&
-    !$omp& private(nNeib,iNeib,dx,dv,vF,lwork,work,iwork,stat,rank,ier,j,k,l,n)
-    do i=1,grid%nE
-      ! list of neighbors
-      if(i>grid%nC)then ! at non-cell
-        if(findCellOnly)then
-          cycle
+    !$omp& private(nNear,iNear,dx,dv,vF,lwork,work,iwork,stat,rank,ier,j)
+    do i=1,grid%nC
+      ! process the list of nearby points
+      nNear=0
+      do j=1,size(grid%near,1)
+        if(grid%near(j,i)>0.and.grid%near(j,i)<=merge(grid%nC,grid%nE,useCellOnly))then
+          nNear=nNear+1
+          iNear(nNear)=grid%near(j,i)
+        else if(grid%near(j,i)==0)then
+          exit
         end if
-        write(*,*)'findGradPolyVect: support cell element only'
-        ! TODO nNeib,lNeib at non-cell
-        ! TODO check useCellOnly
-      else ! at cell
-        nNeib=0
-        do j=1,nF(grid%sE(i))
-          if(nNeib<MAX_N_NEIB.and.grid%neib(j,i)>0&
-          &  .and.(grid%neib(j,i)<=grid%nC.or..not.useCellOnly))then
-            nNeib=nNeib+1
-            iNeib(nNeib)=grid%neib(j,i)
-          end if
-        end do
-        ! find neighbors if needed
-        do while(nNeib<MIN_N_NEIB)
-          n=nNeib
-          do k=1,n
-            l=iNeib(k)
-            if(l<=grid%nC)then
-              do j=1,nF(grid%sE(l))
-                if(nNeib<MAX_N_NEIB.and.grid%neib(j,l)>0&
-                &  .and.(grid%neib(j,l)<=grid%nC.or..not.useCellOnly)&
-                &  .and.all(iNeib(1:nNeib)/=grid%neib(j,l))&
-                &  .and.grid%neib(j,l)/=i)then
-                  nNeib=nNeib+1
-                  iNeib(nNeib)=grid%neib(j,l)
-                end if
-              end do
-            end if
-          end do
-        end do
-      end if
+      end do
       ! LSQ method
-      do j=1,nNeib
-        if(iNeib(j)<=grid%nC)then
-          dx(j,:)=grid%p(:,iNeib(j))-grid%p(:,i)
+      do j=1,nNear
+        if(iNear(j)<=grid%nC)then
+          dx(j,:)=grid%p(:,iNear(j))-grid%p(:,i)
         else
-          vF=n3p(grid%pN(:,grid%iNE(1:3,iNeib(j))))
-          dx(j,:)=2d0*vF(:)*dot_product(grid%p(:,iNeib(j))-grid%p(:,i),vF(:))
+          vF=n3p(grid%pN(:,grid%iNE(1:3,iNear(j))))
+          dx(j,:)=2d0*vF(:)*dot_product(grid%p(:,iNear(j))-grid%p(:,i),vF(:))
         end if
-        dv(j,:)=v(:,iNeib(j))-v(:,i)
+        dv(j,:)=v(:,iNear(j))-v(:,i)
         dv(j,:)=dv(j,:)/dot_product(dx(j,:),dx(j,:))
         dx(j,:)=dx(j,:)/dot_product(dx(j,:),dx(j,:))
       end do
       rcond=-1d0
       lwork=-1
-      call DGELSD(nNeib,DIMS,m,dx,MAX_N_NEIB,dv,MAX_N_NEIB,stat,rcond,rank,work,lwork,iwork,ier)
+      call DGELSD(nNear,DIMS,m,dx,MAX_N_NEAR,dv,MAX_N_NEAR,stat,rcond,rank,work,lwork,iwork,ier)
       lwork=min(MAX_L_WORK,int(work(1)))
-      call DGELSD(nNeib,DIMS,m,dx,MAX_N_NEIB,dv,MAX_N_NEIB,stat,rcond,rank,work,lwork,iwork,ier)
+      call DGELSD(nNear,DIMS,m,dx,MAX_N_NEAR,dv,MAX_N_NEAR,stat,rcond,rank,work,lwork,iwork,ier)
       gradv(:,:,i)=dv(1:DIMS,:)
     end do
     !$end omp parallel do
