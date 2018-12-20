9b9907e7ff4425946eb96505409f2a514eddf07e
Mianzhi Wang <wangmianzhi1@linuxmail.org>
2018-12-20 09:18:28 -0500

    + let's have another try on PBC

--- src/fopbc/modPbc.f90
+++ src/fopbc/modPbc.f90
@@ -5,13 +5,31 @@ module modPbc
   use modPolyFvGrid
   use modCondition
   use modUDF
+  use modNumerics
   use iso_c_binding
   
   public
   
   integer,parameter::DIMS=3 !< three dimensions
   
+  integer,parameter::BC_WALL_TEMP=0 !< wall boundary with prescribed temperature
+  integer,parameter::BC_WALL_TEMP_UDF=5 !< wall boundary with prescribed temperature by UDF
+  integer,parameter::BC_WALL_FLUX=1 !< wall boundary with prescribed heat flux
+  integer,parameter::BC_WALL_FLUX_UDF=6 !< wall boundary with prescribed heat flux by UDF
+  integer,parameter::BC_IN_STATIC=10 !< inflow boundary with static properties
+  integer,parameter::BC_IN_STATIC_UDF=15 !< inflow boundary with static properties by UDF
+  integer,parameter::BC_IN_TOTAL=11 !< inflow boundary with total properties
+  integer,parameter::BC_IN_TOTAL_UDF=16 !< inflow boundary with total properties by UDF
+  integer,parameter::BC_OUT=20 !< outflow boundary
+  integer,parameter::BC_OUT_UDF=25 !< outflow boundary by UDF
+  integer,parameter::BC_FAR=30 !< far-field boundary
+  integer,parameter::BC_FAR_UDF=35 !< far-field boundary by UDF
+  
   type(polyFvGrid)::grid !< computational grid
+  type(condTab)::bc !< boundary conditions
+  type(UDFTab)::udf !< UDF
+  
+  integer,allocatable::iBC(:) !< indexes boundary conditions
   
   double precision::t !< time
   double precision::tFinal !< final time
@@ -20,6 +38,9 @@ module modPbc
   double precision::tNext !< time for next output
   integer::iOut !< index of output
   
+  double precision::Rgas !< specific gas constant [J/kg/K]
+  double precision::gamm !< gamma of gas
+  
   double precision,allocatable::rho(:) !< density [kg/m^3]
   double precision,allocatable::rhou(:,:) !< momentum [kg/s/m^2]
   double precision,allocatable::rhoE(:) !< total energy [J/m^3]
@@ -27,6 +48,7 @@ module modPbc
   double precision,allocatable::u(:,:) !< velocity [m/s]
   double precision,allocatable::temp(:) !< temperature [K]
   double precision,allocatable::Y(:,:) !< mass fraction of species
+  
   ! state at the beginning of a time step
   double precision,allocatable::rho0(:),rhou0(:,:),rhoE0(:),p0(:),u0(:,:),temp0(:),Y0(:,:)
   
@@ -36,17 +58,19 @@ module modPbc
   double precision,allocatable::viscF(:,:) !< viscous force applied on cell [N]
   double precision,allocatable::presF(:,:) !< pressure force applied on cell [N]
   double precision,allocatable::condQ(:) !< heat conduction into cell [W]
-  double precision,allocatable::carrier(:,:) !< {rho,rhou,rhoH} of each cell [*]
-  double precision,allocatable::flux(:,:,:) !< flux of {rho,rhou,rhoH} [*m/s]
-  double precision,allocatable::flow(:,:) !< flow rate of {rho,rhou,rhoH} into cell [*m^3/s]
-  double precision,allocatable::localPrec(:,:,:) !< local preconditioner and its invertion
   
-  double precision,allocatable::x(:) !< solution vector of the nonlinear system of equations
-  double precision,allocatable::xscale(:) !< scaling factors for the solution
-  double precision,allocatable::rscale(:) !< scaling factors for the residual
-  integer(C_LONG)::nEq !< number of equations
-  integer(C_LONG)::ioutFKIN(100) !< integer output of FKINSOL
-  double precision::routFKIN(100) !< real output of FKINSOL
+  ! scales
+  double precision,allocatable::rhoScale !< density scale [kg/m^3]
+  double precision,allocatable::rhouScale !< momentum scale [kg/s/m^2]
+  double precision,allocatable::rhoEScale !< total energy scale [J/m^3]
+  double precision,allocatable::pScale !< pressure scale [Pa]
+  double precision,allocatable::uScale !< velocity scale [m/s]
+  double precision,allocatable::tempScale !< temperature scale [K]
+  
+  ! data for algebraic solver
+  type(NewtonKrylov)::pbcEq !< momentum and pressure equations (isothermal) solved by Newton-GMRES
+  type(fixPt)::energyEq !< energy equation as a fix point problem
+  integer::nItPBC,nItEnergy !< number of iterations
   
 contains
   
@@ -54,85 +78,78 @@ contains
   subroutine init()
     use modFileIO
     integer,parameter::FID=10
-    double precision::pInit,uInit(DIMS),TInit,pE(DIMS)!,tmpD
+    double precision::pInit,uInit(DIMS),TInit,pE(DIMS),tmpD
     double precision,allocatable::YInit(:)
-    integer::ier!,udfIc,udfBc,iUdf(5)
+    integer::ier,udfIc,udfBc,iUdf(5)
     
     ! read inputs
     open(FID,file='grid.msh',action='read')
-    call readGMSH(FID,grid)
+      call readGMSH(FID,grid)
+    close(FID)
     call grid%up()
+    open(FID,file='bc',action='read')
+      call readCondTab(FID,bc)
     close(FID)
-    !open(FID,file='bc',action='read')
-    !call readCondTab(FID,bc)
-    !if(any(bc%t(:)==BC_IN_STATIC_UDF).or.&
-    !&  any(bc%t(:)==BC_IN_TOTAL_UDF).or.&
-    !&  any(bc%t(:)==BC_OUT_UDF).or.&
-    !&  any(bc%t(:)==BC_FAR_UDF))then
-    !  udfBc=1
-    !else
-    !  udfBc=0
-    !end if
-    !close(FID)
-    !open(FID,file='ic',action='read')
-    !read(FID,*),udfIC
-    !if(udfIc==0)then
-    !  read(FID,*),pInit
-    !  read(FID,*),TInit
-    !  read(FID,*),uInit(1)
-    !  read(FID,*),uInit(2)
-    !  read(FID,*),uInit(3)
-    !else
-    !  read(FID,*),tmpD
-    !  iUdf(1)=int(tmpD)
-    !  read(FID,*),tmpD
-    !  iUdf(2)=int(tmpD)
-    !  read(FID,*),tmpD
-    !  iUdf(3)=int(tmpD)
-    !  read(FID,*),tmpD
-    !  iUdf(4)=int(tmpD)
-    !  read(FID,*),tmpD
-    !  iUdf(5)=int(tmpD)
-    !end if
-    !close(FID)
+    call mapCondTab(grid,bc,iBC)
+    if(any(bc%t(:)==BC_WALL_TEMP_UDF).or.&
+    &  any(bc%t(:)==BC_WALL_FLUX_UDF).or.&
+    &  any(bc%t(:)==BC_IN_STATIC_UDF).or.&
+    &  any(bc%t(:)==BC_IN_TOTAL_UDF).or.&
+    &  any(bc%t(:)==BC_OUT_UDF).or.&
+    &  any(bc%t(:)==BC_FAR_UDF))then
+      udfBc=1
+    else
+      udfBc=0
+    end if
+    open(FID,file='ic',action='read')
+      read(FID,*)udfIC
+      if(udfIc==0)then
+        read(FID,*)pInit
+        read(FID,*)TInit
+        read(FID,*)uInit(1)
+        read(FID,*)uInit(2)
+        read(FID,*)uInit(3)
+      else
+        read(FID,*)tmpD
+        iUdf(1)=int(tmpD)
+        read(FID,*)tmpD
+        iUdf(2)=int(tmpD)
+        read(FID,*)tmpD
+        iUdf(3)=int(tmpD)
+        read(FID,*)tmpD
+        iUdf(4)=int(tmpD)
+        read(FID,*)tmpD
+        iUdf(5)=int(tmpD)
+      end if
+    close(FID)
     open(FID,file='sim',action='read')
-    read(FID,*)tFinal
-    read(FID,*)tInt
+      read(FID,*)tFinal
+      read(FID,*)dt
+      read(FID,*)tInt
     close(FID)
-    !open(FID,file='fl',action='read')
-    !read(FID,*),r
-    !read(FID,*),gamm
-    !close(FID)
-    !if(udfIc/=0.or.udfBc/=0)then
-    !  open(FID,file='udf',action='read')
-    !  call readUDFTab(FID,udf)
-    !  close(FID)
-    !end if
-    ! indexes of boundary conditions
-    !allocate(iBC(grid%nE))
-    !iBC(:)=0
-    !do i=1,grid%nE
-    !  do j=1,size(bc%gid)
-    !    if(grid%gid(i)==bc%gid(j))then
-    !      iBC(i)=j
-    !      exit
-    !    end if
-    !  end do
-    !end do
+    open(FID,file='fl',action='read')
+      read(FID,*)Rgas
+      read(FID,*)gamm
+    close(FID)
+    if(udfIc/=0.or.udfBc/=0)then
+      open(FID,file='udf',action='read')
+        call readUDFTab(FID,udf)
+      close(FID)
+    end if
     ! initialize nonlinear solver
-    nEq=5*grid%nC
-    call fnvinits(3,nEq,ier)
-    call fkinmalloc(ioutFKIN,routFKIN,ier)
+    !nEq=5*grid%nC
+    !call fnvinits(3,nEq,ier)
+    !call fkinmalloc(ioutFKIN,routFKIN,ier)
     !call fkinspgmr(0,0,ier)
     !call fkinspilssetprec(1,ier)
-    call fkindense(nEq,ier)
-    call fkinsetrin('MAX_STEP',huge(1d0),ier)
+    !call fkindense(nEq,ier)
+    !call fkinsetrin('MAX_STEP',huge(1d0),ier)
     !call fkinsetrin('FNORM_TOL',1d-5,ier)
     !call fkinsetrin('SSTEP_TOL',1d-9,ier)
-    call fkinsetiin('PRNT_LEVEL',1,ier)
-    allocate(x(nEq))
-    allocate(xscale(nEq))
-    allocate(rscale(nEq))
+    !call fkinsetiin('PRNT_LEVEL',1,ier)
+    !allocate(x(nEq))
+    !allocate(xscale(nEq))
+    !allocate(rscale(nEq))
     ! work space and initial state
     allocate(rho(grid%nE))
     allocate(rho0(grid%nE))
@@ -156,27 +173,18 @@ contains
     allocate(viscF(DIMS,grid%nC))
     allocate(presF(DIMS,grid%nC))
     allocate(condQ(grid%nC))
-    allocate(carrier(5,grid%nE))
-    allocate(flux(DIMS,5,grid%nE))
-    allocate(flow(5,grid%nC))
-    allocate(localPrec(5,5,grid%nC))
     do i=1,grid%nE
-    !  if(udfIc/=0)then
-    !    pE(:)=grid%p(:,i)
-    !    pInit=udf%eval(iUdf(1),pE,0d0)
-    !    TInit=udf%eval(iUdf(2),pE,0d0)
-    !    uInit(1)=udf%eval(iUdf(3),pE,0d0)
-    !    uInit(2)=udf%eval(iUdf(4),pE,0d0)
-    !    uInit(3)=udf%eval(iUdf(5),pE,0d0)
-    !  end if
+      if(udfIc/=0)then
+        pE(:)=grid%p(:,i)
+        pInit=udf%eval(iUdf(1),pE,0d0)
+        TInit=udf%eval(iUdf(2),pE,0d0)
+        uInit(1)=udf%eval(iUdf(3),pE,0d0)
+        uInit(2)=udf%eval(iUdf(4),pE,0d0)
+        uInit(3)=udf%eval(iUdf(5),pE,0d0)
+      end if
       ! FIXME remove below
-      pE(:)=grid%p(:,i)
-      pInit=min(max(0.9d5,-0.5d5*(pE(3)-0.5d0)+0.95d5),1d5)
-      uInit(:)=[0d0,0d0,0d0]
-      TInit=298d0
       YInit=[1d0]
       ! FIXME remove above
-      ! FIXME calculation of rho, rhoE based on cantera
       p(i)=pInit
       u(:,i)=uInit(:)
       temp(i)=TInit
@@ -192,7 +200,6 @@ contains
   !> clear the simulation environment
   subroutine clear()
     call grid%clear()
-    call fkinfree()
   end subroutine
   
   !> record {rho,rhoU,rhoE,p,u,temp,Y} in {rho0,rhoU0,rhoE0,p0,u0,temp0,Y0}
@@ -282,39 +289,20 @@ contains
   
   !> calculate time step size, scaling vectors and initial solution vector
   subroutine preSolve()
-    double precision::ps,us,temps,rhos,rhous,rhoEs
     double precision,parameter::CFL_ACCOUSTIC=10d0
     double precision,parameter::CFL_FLOW=0.5d0
     double precision,parameter::CFL_DIFFUSION=0.5d0
     
     ! TODO calculate dt, cantera sound speed
-    dt=min(tNext-t,minval(CFL_ACCOUSTIC*grid%v(:)**(1d0/3d0)&
-    &                     /sqrt(1.4d0*p(1:grid%nC)/rho(1:grid%nC))))
+    dt=min(dt,minval(CFL_ACCOUSTIC*grid%v(:)**(1d0/3d0)&
+    &                     /sqrt(gamm*p(1:grid%nC)/rho(1:grid%nC))))
     dt=min(dt,minval(CFL_FLOW*grid%v(:)**(1d0/3d0)&
     &                /norm2(u(:,1:grid%nC),1)))
     dt=min(dt,minval(CFL_DIFFUSION*grid%v(:)**(2d0/3d0)&
     &                /(visc(1:grid%nC)/rho(1:grid%nC))))
     dt=1d-4
     
-    ! estimate the solution vector
-    x(:)=0d0
-    
-    ! solution and residual scaling
-    ps=max(maxval(p)-minval(p),maxval(0.5d0*rho*norm2(u,1)**2),1d0)
-    us=max(maxval(norm2(u,1)),sqrt(2d0*ps/minval(rho)))
-    temps=max(maxval(temp)-minval(temp),ps/minval(rho)/287.058d0)
-    rhos=maxval(rho)
-    rhous=rhos*us
-    rhoEs=max(rhos*287.058*temps,0.5d0*rhos*us**2)
-    do i=1,grid%nC
-      j=(i-1)*5
-      xscale(j+1)=1d0/ps
-      xscale(j+2:j+4)=1d0/us
-      xscale(j+5)=1d0/temps
-      rscale(j+1)=1d0/rhos
-      rscale(j+2:j+4)=1d0/rhous
-      rscale(j+5)=1d0/rhoEs
-    end do
+    write(*,'(a,g12.6,a,g12.6)')'[i] starting step, t: ',t,' dt: ',dt
   end subroutine
   
   !> set the boundary conditions
