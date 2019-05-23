7f4c5f68dc16682d16d7d9a39498c45d20cb2081
Mianzhi Wang <wangmianzhi1@linuxmail.org>
2019-05-23 18:15:50 -0400

    - the old boundary viscous traction is more robust

--- src/libfosolvers/FVM/Newtonian.f90
+++ src/libfosolvers/FVM/Newtonian.f90
@@ -66,17 +66,14 @@ contains
         if(m<=grid%nC.and.n<=grid%nC)then ! internal pairs
           flow(:)=viscF*Afs*((u(:,n)-u(:,m))/dPF&
           &                  -matmul(transpose(gradUF(:,:)),&
-          &                          dot_product(sf,tf)*tf+dot_product(sf,rf)*rf))&
-          &       +viscF*grid%aP(i)*[dot_product(gradUF(1,:),grid%normP(:,i)),&
-          &                          dot_product(gradUF(2,:),grid%normP(:,i)),&
-          &                          dot_product(gradUF(3,:),grid%normP(:,i))]&
-          &      -2d0/3d0*viscF*grid%aP(i)*(gradUF(1,1)+gradUF(2,2)+gradUF(3,3))*grid%normP(:,i)
+          &                          dot_product(sf,tf)*tf+dot_product(sf,rf)*rf))
         else ! boundary pairs
-          flow(:)=viscF*grid%aP(i)*&
-          &       (0.5d0*(u(:,n)-u(:,m))&
-          &        -grid%normP(:,i)*dot_product(grid%normP(:,i),0.5d0*(u(:,n)-u(:,m))))&
-          &       /(2d0*dPF*dot_product(sf,grid%normP(:,i)))
+          flow(:)=viscF*Afs*(u(:,n)-u(:,m))/(2d0*dPF)
         end if
+        flow(:)=flow(:)+viscF*grid%aP(i)*[dot_product(gradUF(1,:),grid%normP(:,i)),&
+        &                                 dot_product(gradUF(2,:),grid%normP(:,i)),&
+        &                                 dot_product(gradUF(3,:),grid%normP(:,i))]&
+        &      -2d0/3d0*viscF*grid%aP(i)*(gradUF(1,1)+gradUF(2,2)+gradUF(3,3))*grid%normP(:,i)
         !$omp critical
         if(m<=grid%nC.and.n<=grid%nC)then ! internal pairs
           dRhou(:,m)=dRhou(:,m)+flow(:)
