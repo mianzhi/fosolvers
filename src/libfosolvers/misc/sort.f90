396c47b17865cbb39f0850acfca02629bd42d7da
Mianzhi Wang <wangmianzhi1@linuxmail.org>
2019-12-18 21:47:51 -0500

    + BIG COMMIT: reorder nodes, elements, and pairs for data locality

---
+++ src/libfosolvers/misc/sort.f90
@@ -0,0 +1,65 @@
+!> sorting and reordering
+module modSort
+  private
+  
+  !> generic sort
+  interface sort
+    module procedure::qsortI
+  end interface
+  
+  public::sort
+
+contains
+  
+  !> quicksort integer array (driver)
+  subroutine qsortI(a,perm)
+    integer,intent(inout)::a(:) !< the array
+    integer,intent(inout),optional::perm(*) !< the permutation array
+    
+    if(present(perm))then
+      perm(1:size(a))=[(i,i=1,size(a))]
+      call qsortI_actual(a,perm=perm,first=1,last=size(a))
+    else
+      call qsortI_actual(a,first=1,last=size(a))
+    end if
+  end subroutine
+  
+  !> quicksort integer array (actual)
+  subroutine qsortI_actual(a,perm,first,last)
+    integer,intent(inout)::a(*) !< the array
+    integer,intent(inout),optional::perm(*) !< the permutation array
+    integer,intent(in)::first,last !< sort section from first to last
+    integer::tmp,pivot
+    
+    pivot=a((first+last)/2)
+    i=first
+    j=last
+    do while(.true.)
+      do while(a(i)<pivot)
+        i=i+1
+      end do
+      do while(pivot<a(j))
+        j=j-1
+      end do
+      if(i>=j) exit
+      tmp=a(i)
+      a(i)=a(j)
+      a(j)=tmp
+      if(present(perm))then
+        tmp=perm(i)
+        perm(i)=perm(j)
+        perm(j)=tmp
+      end if
+      i=i+1
+      j=j-1
+    end do
+    if(present(perm))then
+      if(first<i-1) call qsortI_actual(a,perm=perm,first=first,last=i-1)
+      if(j+1<last) call qsortI_actual(a,perm=perm,first=j+1,last=last)
+    else
+      if(first<i-1) call qsortI_actual(a,first=first,last=i-1)
+      if(j+1<last) call qsortI_actual(a,first=j+1,last=last)
+    end if
+  end subroutine
+  
+end module
--- src/libfosolvers/polyGrid/polyX.f90
+++ src/libfosolvers/polyGrid/polyX.f90
@@ -61,11 +61,76 @@ contains
     class(polyX),intent(inout)::this !< this polyX
     
     if(.not.this%isUp)then
+      call sortPolyX(this)
+      ! calculate element position
       forall(i=1:this%nE)
         this%p(:,i)=sum(this%pN(:,this%iNE(1:this%nNE(i),i)),2)/dble(this%nNE(i))
       end forall
       this%isUp=.true.
     end if
+  end subroutine
+  
+  !> sort polyX nodes and elements
+  subroutine sortPolyX(grid)
+    use metis_interface
+    use modSort
+    use iso_c_binding
+    class(polyX),intent(inout)::grid !< the polyX
+    integer(kind=idx_t)::options(0:METIS_NOPTIONS-1)
+    integer(kind=idx_t),allocatable::eptr(:),eind(:),xadj(:),adjncy(:),perm(:),iperm(:)
+    type(C_PTR)::xadjptr,adjncyptr
+    integer(kind=idx_t),pointer::fxadj(:),fadjncy(:)
+    integer(C_INT)::ierr
+    double precision,allocatable::pN(:,:)
+    integer,allocatable::a(:),permE(:),sE(:),nNE(:),iNE(:,:)
+    
+    ! reorder nodes with METIS (improves performance of FEM-type applications)
+    allocate(eptr(grid%nE+1))
+    allocate(eind(sum(grid%nNE(:))))
+    eptr(1)=1
+    do i=1,grid%nE
+      eptr(i+1)=eptr(i)+grid%nNE(i)
+      eind(eptr(i):eptr(i+1)-1)=grid%iNE(1:grid%nNE(i),i)
+    end do
+    ierr=METIS_MeshToNodal(int(grid%nE,kind=idx_t),int(grid%nN,kind=idx_t),eptr,eind,&
+    &                      int(1,kind=idx_t),xadjptr,adjncyptr)
+    call c_f_pointer(xadjptr,fxadj,shape=[grid%nN+1])
+    call c_f_pointer(adjncyptr,fadjncy,shape=[fxadj(grid%nN+1)])
+    allocate(xadj(grid%nN+1))
+    allocate(adjncy(fxadj(grid%nN+1)))
+    allocate(perm(grid%nN))
+    allocate(iperm(grid%nN))
+    xadj(:)=fxadj(1:grid%nN+1)
+    adjncy(:)=fadjncy(1:fxadj(grid%nN+1))
+    ierr=METIS_SetDefaultOptions(options)
+    options(METIS_OPTION_NUMBERING)=1
+    ierr=METIS_NodeND(int(grid%nN,kind=idx_t),xadj,adjncy,options=options,perm=perm,iperm=iperm)
+    allocate(pN(DIMS,grid%nN))
+    pN=grid%pN(:,perm(:))
+    grid%pN(:,:)=pN(:,:)
+    forall(i=1:grid%nE)
+      grid%iNE(1:grid%nNE(i),i)=iperm(grid%iNE(1:grid%nNE(i),i))
+    end forall
+    deallocate(eptr,eind,xadj,adjncy,perm,iperm,pN)
+    ierr=METIS_Free(xadjptr)
+    ierr=METIS_Free(adjncyptr)
+    ! reorder elements for data locality
+    allocate(a(grid%nE))
+    allocate(permE(grid%nE))
+    a(:)=minval(grid%iNE(:,:),dim=1)
+    call sort(a,perm=permE)
+    allocate(sE(grid%nE))
+    allocate(nNE(grid%nE))
+    allocate(iNE(size(grid%iNE,1),grid%nE))
+    forall(i=1:grid%nE)
+      sE(i)=grid%sE(permE(i))
+      nNE(i)=grid%nNE(permE(i))
+      iNE(:,i)=grid%iNE(:,permE(i))
+    end forall
+    grid%sE(:)=sE(:)
+    grid%nNE(:)=nNE(:)
+    grid%iNE(:,:)=iNE(:,:)
+    deallocate(a,permE,sE,nNE,iNE)
   end subroutine
   
   !> destructor of polyX
